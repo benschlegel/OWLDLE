43a504de609ef8403dd5a22cadc207220388e10e
benschlegel <ben5.schlegel@gmail.com>
2024-09-13 21:24:10 +0200

    refactor: rework types, add zod schema for player

--- types/countries.ts
+++ types/countries.ts
@@ -1,203 +1,208 @@
-export type CountryCode =
-	| 'AF' // Afghanistan
-	| 'AL' // Albania
-	| 'DZ' // Algeria
-	| 'AS' // American Samoa
-	| 'AD' // Andorra
-	| 'AO' // Angola
-	| 'AI' // Anguilla
-	| 'AQ' // Antarctica
-	| 'AG' // Antigua and Barbuda
-	| 'AR' // Argentina
-	| 'AM' // Armenia
-	| 'AW' // Aruba
-	| 'AU' // Australia
-	| 'AT' // Austria
-	| 'AZ' // Azerbaijan
-	| 'BS' // Bahamas
-	| 'BH' // Bahrain
-	| 'BD' // Bangladesh
-	| 'BB' // Barbados
-	| 'BY' // Belarus
-	| 'BE' // Belgium
-	| 'BZ' // Belize
-	| 'BJ' // Benin
-	| 'BM' // Bermuda
-	| 'BT' // Bhutan
-	| 'BO' // Bolivia
-	| 'BA' // Bosnia and Herzegovina
-	| 'BW' // Botswana
-	| 'BR' // Brazil
-	| 'IO' // British Indian Ocean Territory
-	| 'BN' // Brunei
-	| 'BG' // Bulgaria
-	| 'BF' // Burkina Faso
-	| 'BI' // Burundi
-	| 'CV' // Cabo Verde
-	| 'KH' // Cambodia
-	| 'CM' // Cameroon
-	| 'CA' // Canada
-	| 'KY' // Cayman Islands
-	| 'CF' // Central African Republic
-	| 'TD' // Chad
-	| 'CL' // Chile
-	| 'CN' // China
-	| 'CO' // Colombia
-	| 'KM' // Comoros
-	| 'CG' // Congo (Congo-Brazzaville)
-	| 'CD' // Congo (Congo-Kinshasa)
-	| 'CR' // Costa Rica
-	| 'CI' // Côte d'Ivoire
-	| 'HR' // Croatia
-	| 'CU' // Cuba
-	| 'CY' // Cyprus
-	| 'CZ' // Czech Republic
-	| 'DK' // Denmark
-	| 'DJ' // Djibouti
-	| 'DM' // Dominica
-	| 'DO' // Dominican Republic
-	| 'EC' // Ecuador
-	| 'EG' // Egypt
-	| 'SV' // El Salvador
-	| 'GQ' // Equatorial Guinea
-	| 'ER' // Eritrea
-	| 'EE' // Estonia
-	| 'SZ' // Eswatini
-	| 'ET' // Ethiopia
-	| 'FJ' // Fiji
-	| 'FI' // Finland
-	| 'FR' // France
-	| 'GA' // Gabon
-	| 'GM' // Gambia
-	| 'GE' // Georgia
-	| 'DE' // Germany
-	| 'GH' // Ghana
-	| 'GR' // Greece
-	| 'GD' // Grenada
-	| 'GU' // Guam
-	| 'GT' // Guatemala
-	| 'GN' // Guinea
-	| 'GW' // Guinea-Bissau
-	| 'GY' // Guyana
-	| 'HT' // Haiti
-	| 'HN' // Honduras
-	| 'HU' // Hungary
-	| 'IS' // Iceland
-	| 'IN' // India
-	| 'ID' // Indonesia
-	| 'IR' // Iran
-	| 'IQ' // Iraq
-	| 'IE' // Ireland
-	| 'IL' // Israel
-	| 'IT' // Italy
-	| 'JM' // Jamaica
-	| 'JP' // Japan
-	| 'JO' // Jordan
-	| 'KZ' // Kazakhstan
-	| 'KE' // Kenya
-	| 'KI' // Kiribati
-	| 'KP' // North Korea
-	| 'KR' // South Korea
-	| 'KW' // Kuwait
-	| 'KG' // Kyrgyzstan
-	| 'LA' // Laos
-	| 'LV' // Latvia
-	| 'LB' // Lebanon
-	| 'LS' // Lesotho
-	| 'LR' // Liberia
-	| 'LY' // Libya
-	| 'LI' // Liechtenstein
-	| 'LT' // Lithuania
-	| 'LU' // Luxembourg
-	| 'MG' // Madagascar
-	| 'MW' // Malawi
-	| 'MY' // Malaysia
-	| 'MV' // Maldives
-	| 'ML' // Mali
-	| 'MT' // Malta
-	| 'MH' // Marshall Islands
-	| 'MR' // Mauritania
-	| 'MU' // Mauritius
-	| 'MX' // Mexico
-	| 'FM' // Micronesia
-	| 'MD' // Moldova
-	| 'MC' // Monaco
-	| 'MN' // Mongolia
-	| 'ME' // Montenegro
-	| 'MA' // Morocco
-	| 'MZ' // Mozambique
-	| 'MM' // Myanmar (Burma)
-	| 'NA' // Namibia
-	| 'NR' // Nauru
-	| 'NP' // Nepal
-	| 'NL' // Netherlands
-	| 'NZ' // New Zealand
-	| 'NI' // Nicaragua
-	| 'NE' // Niger
-	| 'NG' // Nigeria
-	| 'MK' // North Macedonia
-	| 'NO' // Norway
-	| 'OM' // Oman
-	| 'PK' // Pakistan
-	| 'PW' // Palau
-	| 'PA' // Panama
-	| 'PG' // Papua New Guinea
-	| 'PY' // Paraguay
-	| 'PE' // Peru
-	| 'PH' // Philippines
-	| 'PL' // Poland
-	| 'PT' // Portugal
-	| 'QA' // Qatar
-	| 'RO' // Romania
-	| 'RU' // Russia
-	| 'RW' // Rwanda
-	| 'KN' // Saint Kitts and Nevis
-	| 'LC' // Saint Lucia
-	| 'VC' // Saint Vincent and the Grenadines
-	| 'WS' // Samoa
-	| 'SM' // San Marino
-	| 'ST' // São Tomé and Príncipe
-	| 'SA' // Saudi Arabia
-	| 'SN' // Senegal
-	| 'RS' // Serbia
-	| 'SC' // Seychelles
-	| 'SL' // Sierra Leone
-	| 'SG' // Singapore
-	| 'SK' // Slovakia
-	| 'SI' // Slovenia
-	| 'SB' // Solomon Islands
-	| 'SO' // Somalia
-	| 'ZA' // South Africa
-	| 'SS' // South Sudan
-	| 'ES' // Spain
-	| 'LK' // Sri Lanka
-	| 'SD' // Sudan
-	| 'SR' // Suriname
-	| 'SE' // Sweden
-	| 'CH' // Switzerland
-	| 'SY' // Syria
-	| 'TW' // Taiwan
-	| 'TJ' // Tajikistan
-	| 'TZ' // Tanzania
-	| 'TH' // Thailand
-	| 'TL' // Timor-Leste
-	| 'TG' // Togo
-	| 'TO' // Tonga
-	| 'TT' // Trinidad and Tobago
-	| 'TN' // Tunisia
-	| 'TR' // Turkey
-	| 'TM' // Turkmenistan
-	| 'TV' // Tuvalu
-	| 'UG' // Uganda
-	| 'UA' // Ukraine
-	| 'AE' // United Arab Emirates
-	| 'GB' // United Kingdom
-	| 'US' // United States
-	| 'UY' // Uruguay
-	| 'UZ' // Uzbekistan
-	| 'VU' // Vanuatu
-	| 'VE' // Venezuela
-	| 'VN' // Vietnam
-	| 'YE' // Yemen
-	| 'ZM' // Zambia
-	| 'ZW'; // Zimbabwe
+// https://vectorflags.s3.amazonaws.com/flags/kr-square-01.png
+
+export type CountryCode = (typeof countries)[number];
+
+export const countries = [
+	'AF', // Afghanistan
+	'AL', // Albania
+	'DZ', // Algeria
+	'AS', // American Samoa
+	'AD', // Andorra
+	'AO', // Angola
+	'AI', // Anguilla
+	'AQ', // Antarctica
+	'AG', // Antigua and Barbuda
+	'AR', // Argentina
+	'AM', // Armenia
+	'AW', // Aruba
+	'AU', // Australia
+	'AT', // Austria
+	'AZ', // Azerbaijan
+	'BS', // Bahamas
+	'BH', // Bahrain
+	'BD', // Bangladesh
+	'BB', // Barbados
+	'BY', // Belarus
+	'BE', // Belgium
+	'BZ', // Belize
+	'BJ', // Benin
+	'BM', // Bermuda
+	'BT', // Bhutan
+	'BO', // Bolivia
+	'BA', // Bosnia and Herzegovina
+	'BW', // Botswana
+	'BR', // Brazil
+	'IO', // British Indian Ocean Territory
+	'BN', // Brunei
+	'BG', // Bulgaria
+	'BF', // Burkina Faso
+	'BI', // Burundi
+	'CV', // Cabo Verde
+	'KH', // Cambodia
+	'CM', // Cameroon
+	'CA', // Canada
+	'KY', // Cayman Islands
+	'CF', // Central African Republic
+	'TD', // Chad
+	'CL', // Chile
+	'CN', // China
+	'CO', // Colombia
+	'KM', // Comoros
+	'CG', // Congo (Congo-Brazzaville)
+	'CD', // Congo (Congo-Kinshasa)
+	'CR', // Costa Rica
+	'CI', // Côte d'Ivoire
+	'HR', // Croatia
+	'CU', // Cuba
+	'CY', // Cyprus
+	'CZ', // Czech Republic
+	'DK', // Denmark
+	'DJ', // Djibouti
+	'DM', // Dominica
+	'DO', // Dominican Republic
+	'EC', // Ecuador
+	'EG', // Egypt
+	'SV', // El Salvador
+	'GQ', // Equatorial Guinea
+	'ER', // Eritrea
+	'EE', // Estonia
+	'SZ', // Eswatini
+	'ET', // Ethiopia
+	'FJ', // Fiji
+	'FI', // Finland
+	'FR', // France
+	'GA', // Gabon
+	'GM', // Gambia
+	'GE', // Georgia
+	'DE', // Germany
+	'GH', // Ghana
+	'GR', // Greece
+	'GD', // Grenada
+	'GU', // Guam
+	'GT', // Guatemala
+	'GN', // Guinea
+	'GW', // Guinea-Bissau
+	'GY', // Guyana
+	'HT', // Haiti
+	'HN', // Honduras
+	'HU', // Hungary
+	'IS', // Iceland
+	'IN', // India
+	'ID', // Indonesia
+	'IR', // Iran
+	'IQ', // Iraq
+	'IE', // Ireland
+	'IL', // Israel
+	'IT', // Italy
+	'JM', // Jamaica
+	'JP', // Japan
+	'JO', // Jordan
+	'KZ', // Kazakhstan
+	'KE', // Kenya
+	'KI', // Kiribati
+	'KP', // North Korea
+	'KR', // South Korea
+	'KW', // Kuwait
+	'KG', // Kyrgyzstan
+	'LA', // Laos
+	'LV', // Latvia
+	'LB', // Lebanon
+	'LS', // Lesotho
+	'LR', // Liberia
+	'LY', // Libya
+	'LI', // Liechtenstein
+	'LT', // Lithuania
+	'LU', // Luxembourg
+	'MG', // Madagascar
+	'MW', // Malawi
+	'MY', // Malaysia
+	'MV', // Maldives
+	'ML', // Mali
+	'MT', // Malta
+	'MH', // Marshall Islands
+	'MR', // Mauritania
+	'MU', // Mauritius
+	'MX', // Mexico
+	'FM', // Micronesia
+	'MD', // Moldova
+	'MC', // Monaco
+	'MN', // Mongolia
+	'ME', // Montenegro
+	'MA', // Morocco
+	'MZ', // Mozambique
+	'MM', // Myanmar (Burma)
+	'NA', // Namibia
+	'NR', // Nauru
+	'NP', // Nepal
+	'NL', // Netherlands
+	'NZ', // New Zealand
+	'NI', // Nicaragua
+	'NE', // Niger
+	'NG', // Nigeria
+	'MK', // North Macedonia
+	'NO', // Norway
+	'OM', // Oman
+	'PK', // Pakistan
+	'PW', // Palau
+	'PA', // Panama
+	'PG', // Papua New Guinea
+	'PY', // Paraguay
+	'PE', // Peru
+	'PH', // Philippines
+	'PL', // Poland
+	'PT', // Portugal
+	'QA', // Qatar
+	'RO', // Romania
+	'RU', // Russia
+	'RW', // Rwanda
+	'KN', // Saint Kitts and Nevis
+	'LC', // Saint Lucia
+	'VC', // Saint Vincent and the Grenadines
+	'WS', // Samoa
+	'SM', // San Marino
+	'ST', // São Tomé and Príncipe
+	'SA', // Saudi Arabia
+	'SN', // Senegal
+	'RS', // Serbia
+	'SC', // Seychelles
+	'SL', // Sierra Leone
+	'SG', // Singapore
+	'SK', // Slovakia
+	'SI', // Slovenia
+	'SB', // Solomon Islands
+	'SO', // Somalia
+	'ZA', // South Africa
+	'SS', // South Sudan
+	'ES', // Spain
+	'LK', // Sri Lanka
+	'SD', // Sudan
+	'SR', // Suriname
+	'SE', // Sweden
+	'CH', // Switzerland
+	'SY', // Syria
+	'TW', // Taiwan
+	'TJ', // Tajikistan
+	'TZ', // Tanzania
+	'TH', // Thailand
+	'TL', // Timor-Leste
+	'TG', // Togo
+	'TO', // Tonga
+	'TT', // Trinidad and Tobago
+	'TN', // Tunisia
+	'TR', // Turkey
+	'TM', // Turkmenistan
+	'TV', // Tuvalu
+	'UG', // Uganda
+	'UA', // Ukraine
+	'AE', // United Arab Emirates
+	'GB', // United Kingdom
+	'US', // United States
+	'UY', // Uruguay
+	'UZ', // Uzbekistan
+	'VU', // Vanuatu
+	'VE', // Venezuela
+	'VN', // Vietnam
+	'YE', // Yemen
+	'ZM', // Zambia
+	'ZW', // Zimbabwe
+] as const;
--- types/players.ts
+++ types/players.ts
@@ -1,36 +1,49 @@
-import type { CountryCode } from '@/types/countries';
+import { countries, type CountryCode } from '@/types/countries';
+import { z } from 'zod';
 
 /**
  * Type for player from database
  */
-export type Player = {
+export type Player = z.infer<typeof playerSchema>;
+
+export type PlayerFull = Player & { isEastern: boolean; id: number };
+
+const ROLES = ['Damage', 'Tank', 'Support'] as const;
+
+// Keep types in array to check what region player is from
+const EASTERN = ['BostonUprising', 'FloridaMayhem', 'HoustonOutlaws', 'LondonSpitfire', 'NewYorkExcelsior', 'PhiladelphiaFusion'] as const;
+const WESTERN = ['DallasFuel', 'LosAngelesGladiators', 'LosAngelesValiant', 'SanFranciscoShock', 'SeoulDynasty', 'ShanghaiDragons'] as const;
+
+type EasternTeam = (typeof EASTERN)[number];
+type WesternTeam = (typeof WESTERN)[number];
+type OWLTeam = EasternTeam | WesternTeam;
+
+export const playerSchema = z.object({
 	/**
 	 * Player name
 	 */
-	name: string;
+	name: z.string(),
 	/**
 	 * Role of player
 	 */
-	role: Role;
+	role: z.enum(ROLES),
 	/**
 	 * What country player is from
 	 */
-	country: CountryCode;
+	country: z.enum(countries),
 	/**
 	 * What team the player is from
 	 */
-	team: OWLTeam;
-};
-
-type Role = 'Damage' | 'Tank' | 'Support';
-
-// Keep types in array to check what region player is from
-const eastern = ['BostonUprising', 'FloridaMayhem', 'HoustonOutlaws', 'LondonSpitfire', 'NewYorkExcelsior', 'PhiladelphiaFusion'] as const;
-const western = ['DallasFuel', 'LosAngelesGladiators', 'LosAngelesValiant', 'SanFranciscoShock', 'SeoulDynasty', 'ShanghaiDragons'] as const;
-
-type EasternTeam = (typeof eastern)[number];
-type WesternTeam = (typeof western)[number];
-type OWLTeam = EasternTeam | WesternTeam;
+	team: z.enum(EASTERN).or(z.enum(WESTERN)),
+	/**
+	 * Wether player is eastern (player is western if false)
+	 */
+	isEastern: z.boolean().optional(),
+	/**
+	 * Player id
+	 */
+	id: z.number().optional(),
+});
 
 /**
  * Checks if a team is Eastern/Western (or Atlantic/Pacific)
@@ -38,5 +51,5 @@ type OWLTeam = EasternTeam | WesternTeam;
  * @returns true, if team is Eastern/Atlantic and false, if the team is Western/Pacific
  */
 export function isEastern(team: OWLTeam): boolean {
-	return eastern.includes(team as unknown as EasternTeam);
+	return EASTERN.includes(team as unknown as EasternTeam);
 }
