583936b18524aa1a32bd39e292d4a6c1c304ee3e
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-13 15:25:25 +0200

    feat: properly implement theme switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const geistSans = localFont({
 	src: './fonts/GeistVF.woff',
@@ -25,7 +26,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
--- components/landing-page/header.tsx
+++ components/landing-page/header.tsx
@@ -1,29 +1,14 @@
-'use client';
-
+import { ModeToggle } from '@/components/theme-switcher';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
-import { InfoIcon, MoonIcon } from 'lucide-react';
-import { useState, useEffect } from 'react';
+import { InfoIcon } from 'lucide-react';
 
 export default function Header() {
-	const [theme, setTheme] = useState<'light' | 'dark' | 'system'>('dark');
-
-	useEffect(() => {
-		// Apply the theme to the document
-		const root = window.document.documentElement;
-		root.classList.remove('light', 'dark');
-		if (theme === 'system') {
-			const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-			root.classList.add(systemTheme);
-		} else {
-			root.classList.add(theme);
-		}
-	}, [theme]);
 	return (
 		<>
 			<div className="flex flex-row justify-between items-center w-full">
 				<Button variant="ghost" size="icon" className="p-0">
-					<InfoIcon className="h-6 w-6" />
+					<InfoIcon className="h-[1.3rem] w-[1.3rem]" />
 				</Button>
 				<div className="mb-3">
 					<h1 className="text-4xl font-bold text-center">
@@ -31,9 +16,7 @@ export default function Header() {
 						<span className="text-primary-foreground/70">S1</span>LE
 					</h1>
 				</div>
-				<Button variant="ghost" size="icon" onClick={() => setTheme('light')}>
-					<MoonIcon className="h-6 w-6" />
-				</Button>
+				<ModeToggle />
 			</div>
 			<Separator className="mb-4 mt-1" />
 		</>
