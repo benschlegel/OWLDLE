6b5f53d62776be9d5c46b28c620e4aea7fe043db
benschlegel <ben5.schlegel@gmail.com>
2024-09-14 01:29:40 +0200

    feat: add startup/instrumentation function

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
 	compiler: {
 		removeConsole: process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false,
 	},
+	experimental: {
+		instrumentationHook: true,
+	},
 };
 
 export default nextConfig;
