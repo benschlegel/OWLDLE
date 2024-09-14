a8b56d9ae669904fdba1cd2336f31c7bd5fe99ca
benschlegel <ben5.schlegel@gmail.com>
2024-09-14 22:23:59 +0200

    test: add local+full test script

--- tests/logos.local.test.ts
+++ tests/logos.local.test.ts
@@ -41,7 +41,7 @@ const hashedTeamLogos: HashedLogo[] = TEAM_LOGOS_S1.map((teamData) => {
 	}
 });
 
-describe('team logos loading (season 1)', () => {
+describe('all team logos loading (season 1)', () => {
 	test.concurrent.each(hashedTeamLogos)('$logo.displayName: logo exists and can be read', async ({ logo, hash }) => {
 		// Convert the public folder URL path into a local file system path
 		const absoluteImagePath = join(__dirname, '../public', logo.imgUrl);
