779782d3aa943908147537033c0345c1f78951eb
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-15 13:16:52 +0200

    feat: add color indicator to game cells

--- components/wordle/GuessRow.tsx
+++ components/wordle/GuessRow.tsx
@@ -1,28 +1,26 @@
 import GameCell from '@/components/wordle/GameCell';
 import type { RowData } from '@/components/wordle/GameContainer';
 import TeamLogo from '@/components/wordle/TeamLogo';
-import type { Player } from '@/types/players';
-import type { GuessResponse } from '@/types/server';
-
 type Props = {
 	data?: RowData;
 };
 
 const cellSize = '3.75rem';
-
 export default function GuessRow({ data }: Props) {
 	return (
 		<div className={`flex flex-row gap-2 w-full h-[3.7rem] transition-colors`}>
 			{/* Player name */}
-			<GameCell isLarge cellSize={cellSize} />
+			<GameCell isLarge cellSize={cellSize} isCorrect={data?.guessResult.isNameCorrect} />
 			{/* Country */}
-			<GameCell cellSize={cellSize} />
+			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isCountryCorrect} />
 			{/* Role */}
-			<GameCell cellSize={cellSize} />
+			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isRoleCorrect} />
 			{/* Region */}
-			<GameCell cellSize={cellSize} />
+			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isRegionCorrect} />
 			{/* Team */}
-			<GameCell cellSize={cellSize}>{/* <TeamLogo teamName="LondonSpitfire" /> */}</GameCell>
+			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isTeamCorrect}>
+				<TeamLogo teamName={data?.player.team} />
+			</GameCell>
 		</div>
 	);
 }
