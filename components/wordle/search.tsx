943ba9fe3cb9e430336730e591e33ec5a1ab1fb3
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-15 13:45:14 +0200

    feat: improve context typing

--- components/wordle/search.tsx
+++ components/wordle/search.tsx
@@ -1,13 +1,19 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { GuessContext } from '@/context/GuessContext';
 import { cn } from '@/lib/utils';
 import { Dices } from 'lucide-react';
+import { useContext } from 'react';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
 	placeholder?: string;
 }
 
 export default function PlayerSearch({ className }: Props) {
+	const [guesses, setGuesses] = useContext(GuessContext);
+
 	return (
 		<div className={cn('relative', className)}>
 			<Input type="search" placeholder="Search..." className="pr-10" />
