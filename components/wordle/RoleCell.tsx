945e7b45d4310cd854b5485bab2590d9fb73e9b7
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-15 15:55:56 +0200

    style: fix role icon style

--- components/wordle/RoleCell.tsx
+++ components/wordle/RoleCell.tsx
@@ -18,7 +18,7 @@ export default function RoleCell({ role, className }: Props) {
 	return (
 		<div className={cn('p-2 flex justify-center items-center', className)}>
 			{/* Apply same styling to all icons */}
-			<RoleIcon className="text-purple-200 fill-foreground h-full w-full" />
+			<RoleIcon className="fill-[#dfdfd7] opacity-95 h-full w-full" />
 		</div>
 	);
 }
