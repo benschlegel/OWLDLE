9ce82e03119d095628fd6d363194382b979665b5
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-15 17:55:42 +0200

    style: fix country cell style

--- components/game-container/CountryCell.tsx
+++ components/game-container/CountryCell.tsx
@@ -8,8 +8,16 @@ export default function CountryCell({ imgSrc }: Props) {
 	if (!imgSrc) return <></>;
 
 	return (
-		<div className="rounded-md flex justify-center items-center m-1 aspect-square">
-			<Image src={imgSrc} alt={'Square country flag'} width={64} height={64} className="p-[0.35rem] h-auto w-auto max-h-full max-w-full" />
+		<div className="rounded-md flex justify-center items-center m-[0.28rem] aspect-square">
+			<Image
+				src={imgSrc}
+				alt={'Square country flag'}
+				width={64}
+				height={64}
+				className="rounded-md h-auto w-auto max-h-full max-w-full"
+				fetchPriority="high"
+				priority
+			/>
 		</div>
 	);
 }
