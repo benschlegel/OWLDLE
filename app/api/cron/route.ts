0b565f3b698a625f68486f4dc6aecfb1bae1506c
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-15 12:36:46 +0200

    feat: add cron job endpoint + vercel config

---
+++ app/api/cron/route.ts
@@ -0,0 +1,14 @@
+import { GET as RouteGet } from '@/app/api/validate/route';
+
+export const dynamic = 'force-dynamic';
+
+export async function GET(request: Request) {
+	// Return forbidden if token was not passed (route should not be publicly accessible)
+	if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+		return new Response('Unauthorized', { status: 401, statusText: 'Unauthorized' });
+	}
+
+	// TODO: Do db stuff
+	const reset = (await (await RouteGet()).json()) as Date;
+	return new Response(`Next reset: ${JSON.stringify(reset)}`);
+}
