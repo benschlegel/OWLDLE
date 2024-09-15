65346e3371fa33889813c6ec142a12316a6487af
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-15 12:13:17 +0200

    feat: add get endpoint to validate for next reset time

--- app/validate/route.ts
+++ app/validate/route.ts
@@ -1,10 +1,17 @@
-import { validateGuess } from '@/lib/server';
+import { addDays, validateGuess } from '@/lib/server';
 import { type Player, playerSchema } from '@/types/players';
 
 export const dynamic = 'force-static';
 
 // TODO: reset current round if server curr player resets
-const CURR_PLAYER: Player = { country: 'US', team: 'DallasFuel', role: 'Damage', name: 'Seagull', isEastern: true, id: 9 };
+const DEFAULT_PLAYER: Player = { country: 'US', team: 'DallasFuel', role: 'Damage', name: 'DEFAULT_PLAYER', isEastern: true, id: 9 };
+const DEFAULT_DATE = new Date('2024-09-15T11:40:00.000+02:00');
+
+let currPlayer: Player = DEFAULT_PLAYER;
+let nextReset: Date = DEFAULT_DATE;
+
+// How many days per "round" of the game (e.g. 1 means the game resets once per day, 2 every two days, etc)
+const DAY_INCREMENT = 1;
 
 /**
  * Verifies a guess
@@ -24,7 +31,19 @@ export async function POST(req: Request) {
 
 	// Raw data
 	const player = playerRes.data as Player;
-	const resResponse = validateGuess(player, CURR_PLAYER);
+	const resResponse = validateGuess(player, currPlayer);
 
 	return Response.json(resResponse);
+}
+
+export async function GET() {
+	if (new Date() >= nextReset) {
+		// fetch from backend
+		nextReset = addDays(nextReset, DAY_INCREMENT);
+		// get new player from backend
+		currPlayer = DEFAULT_PLAYER;
+		// add day
+		// set back to backend
+	}
+	return Response.json({ nextReset: nextReset });
 }
