aa6eeb7b3d17734e84e4f69df9c552b00a3f8580
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-15 14:59:08 +0200

    feat: add tooltips to game cells

--- components/wordle/GameCell.tsx
+++ components/wordle/GameCell.tsx
@@ -1,12 +1,22 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import React, { type PropsWithChildren } from 'react';
 
 type Props = {
 	isLarge?: boolean;
 	isCorrect?: boolean;
-	cellSize: string;
+	cellSize?: string;
+	/**
+	 * Tooltip that will show while cell is empty (e.g. "Role")
+	 */
+	tooltipDescription?: string;
+	/**
+	 * Tooltip that will show after guess was made (e.g. "Playername")
+	 */
+	tooltipGuess?: string;
 };
 
-export default function GameCell({ isLarge = false, isCorrect, cellSize, children }: PropsWithChildren<Props>) {
+export default function GameCell({ isLarge = false, isCorrect, cellSize, children, tooltipDescription, tooltipGuess }: PropsWithChildren<Props>) {
+	// Set background color based on correct value (gray if undefined, green if correct, red if incorrect)
 	let bgColor = 'bg-secondary';
 	if (isCorrect === true) {
 		bgColor = 'bg-green-600';
@@ -14,5 +24,23 @@ export default function GameCell({ isLarge = false, isCorrect, cellSize, childre
 		bgColor = 'bg-red-600';
 	}
 
-	return <div className={`w-[3.7rem] ${bgColor} rounded-sm transition-colors ${isLarge ? 'flex-1' : ''}`}>{children}</div>;
+	// Set tooltip for cell (tooltipDescription if defined, e.g. "correct ${tooltipDescription}" if tooltipGuess undefined)
+	let tooltip = isCorrect === undefined ? tooltipDescription : tooltipGuess;
+	if (tooltipGuess === undefined && isCorrect !== undefined) {
+		const prefix = !isCorrect ? 'Incorrect' : 'Correct';
+		tooltip = `${prefix} ${tooltipDescription}`;
+	}
+
+	return (
+		<>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger asChild tabIndex={0}>
+						<div className={`w-[3.7rem] ${bgColor} rounded-sm transition-colors ${isLarge ? 'flex-1' : ''}`}>{children}</div>
+					</TooltipTrigger>
+					<TooltipContent>{tooltip && tooltip.length > 0 && <p>{tooltip}</p>}</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+		</>
+	);
 }
--- components/wordle/GuessRow.tsx
+++ components/wordle/GuessRow.tsx
@@ -10,15 +10,15 @@ export default function GuessRow({ data }: Props) {
 	return (
 		<div className={`flex flex-row gap-2 w-full h-[3.7rem] transition-colors`}>
 			{/* Player name */}
-			<GameCell isLarge cellSize={cellSize} isCorrect={data?.guessResult.isNameCorrect} />
+			<GameCell isLarge cellSize={cellSize} isCorrect={data?.guessResult.isNameCorrect} tooltipDescription="Player name" />
 			{/* Country */}
-			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isCountryCorrect} />
+			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isCountryCorrect} tooltipDescription="Country" />
 			{/* Role */}
-			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isRoleCorrect} />
+			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isRoleCorrect} tooltipDescription="Role" />
 			{/* Region */}
-			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isRegionCorrect} />
+			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isRegionCorrect} tooltipDescription="Region" />
 			{/* Team */}
-			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isTeamCorrect}>
+			<GameCell cellSize={cellSize} isCorrect={data?.guessResult.isTeamCorrect} tooltipDescription="Team">
 				<TeamLogo teamName={data?.player.team} />
 			</GameCell>
 		</div>
