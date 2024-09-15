9317dc1809617bfc73a6403862bed8331f0c05c3
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-15 19:23:44 +0200

    style: improve mobile style

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/landing-page/header';
 
 export default function Home() {
 	return (
-		<div className="p-6 md:p-8 w-full h-full flex justify-center items-center">
+		<div className="px-4 py-8 md:px-8 w-full h-full flex justify-center items-center">
 			<main className="w-[32rem]">
 				<Header />
 			</main>
