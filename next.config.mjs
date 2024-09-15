b331f5ea4d57952f3fccbb89b49b2d1b2511eacf
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-15 17:26:37 +0200

    feat: add million.js lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,19 @@
+import MillionLint from '@million/lint';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		formats: ['image/avif'],
-	},
-	compiler: {
-		removeConsole: process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false,
-	},
-	experimental: {
-		reactCompiler: true,
-		instrumentationHook: true,
-	},
+  images: {
+    formats: ['image/avif']
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production' ? {
+      exclude: ['error']
+    } : false
+  },
+  experimental: {
+    reactCompiler: true,
+    instrumentationHook: true
+  }
 };
-
-export default nextConfig;
+export default MillionLint.next({
+  rsc: true
+})(nextConfig);
