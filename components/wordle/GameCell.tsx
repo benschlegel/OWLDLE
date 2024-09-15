32f88f7b95752e93ecebd240d72818f7b44c7de0
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-15 13:16:52 +0200

    feat: add color indicator to game cells

--- components/wordle/GameCell.tsx
+++ components/wordle/GameCell.tsx
@@ -2,9 +2,17 @@ import React, { type PropsWithChildren } from 'react';
 
 type Props = {
 	isLarge?: boolean;
+	isCorrect?: boolean;
 	cellSize: string;
 };
 
-export default function GameCell({ isLarge = false, cellSize, children }: PropsWithChildren<Props>) {
-	return <div className={`w-[3.7rem] bg-secondary rounded-sm transition-colors ${isLarge ? 'flex-1' : ''}`}>{children}</div>;
+export default function GameCell({ isLarge = false, isCorrect, cellSize, children }: PropsWithChildren<Props>) {
+	let bgColor = 'bg-secondary';
+	if (isCorrect === true) {
+		bgColor = 'bg-green-600';
+	} else if (isCorrect === false) {
+		bgColor = 'bg-red-600';
+	}
+
+	return <div className={`w-[3.7rem] ${bgColor} rounded-sm transition-colors ${isLarge ? 'flex-1' : ''}`}>{children}</div>;
 }
--- components/wordle/TeamLogo.tsx
+++ components/wordle/TeamLogo.tsx
@@ -4,13 +4,13 @@ import { TEAM_LOGOS_S1 } from '@/data/teams/logos';
 import Image from 'next/image';
 
 type Props = {
-	teamName: Player['team'];
+	teamName?: Player['team'];
 };
 
 export default function TeamLogo({ teamName }: Props) {
 	const team = TEAM_LOGOS_S1.find((t) => t.teamName === teamName);
 
-	if (!team) return <div className={'rounded-md h-32 w-32 bg-black'} />;
+	if (!teamName || !team) return <></>;
 
 	return (
 		<div className="rounded-md flex justify-center items-center m-1 aspect-square" style={{ backgroundColor: team.backgroundColor }}>
