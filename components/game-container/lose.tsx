0b2159c2ed7584b8f33f19d902c88a8c0cf57bb9
benschlegel <ben5.schlegel@gmail.com>
2024-09-16 19:09:54 +0200

    feat: pass backend data to win/loss screens

--- components/game-container/lose.tsx
+++ components/game-container/lose.tsx
@@ -1,3 +1,12 @@
-export default function LossScreen() {
-	return <div>lose</div>;
+type Props = {
+	nextReset: Date;
+	correctPlayer: string;
+};
+
+export default function LossScreen({ correctPlayer, nextReset }: Partial<Props>) {
+	return (
+		<div className="flex p-8 mt-4">
+			<p>{correctPlayer}</p>
+		</div>
+	);
 }
--- components/game-container/win.tsx
+++ components/game-container/win.tsx
@@ -1,3 +1,11 @@
-export default function WinScreen() {
-	return <div>win</div>;
+type Props = {
+	nextReset: Date;
+};
+
+export default function WinScreen({ nextReset }: Partial<Props>) {
+	return (
+		<div className="flex p-8 mt-4">
+			<p>{nextReset?.toString()}</p>
+		</div>
+	);
 }
