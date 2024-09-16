01c2d84cb18f7f4eca62b65108db6a19936670d6
benschlegel <ben5.schlegel@gmail.com>
2024-09-16 19:47:29 +0200

    feat: add countdown component

--- components/game-container/win.tsx
+++ components/game-container/win.tsx
@@ -1,11 +1,35 @@
+'use client';
+import { useEffect, useState } from 'react';
+import Countdown, { type CountdownRenderProps, type CountdownRendererFn } from 'react-countdown';
+
 type Props = {
 	nextReset: Date;
 };
 
-export default function WinScreen({ nextReset }: Partial<Props>) {
+function renderer({ days, hours, minutes, seconds, completed, milliseconds }: CountdownRenderProps) {
+	if (completed) {
+		// Render a completed state
+		return <>Countdown over</>;
+	}
+	// Render a countdown
 	return (
-		<div className="flex p-8 mt-4">
-			<p>{nextReset?.toString()}</p>
+		<p>
+			<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>:<span>{milliseconds}</span>
+		</p>
+	);
+}
+
+export default function WinScreen({ nextReset }: Partial<Props>) {
+	if (nextReset === undefined) return <p />;
+	const [showTimer, setShowTimer] = useState(false);
+
+	useEffect(() => {
+		setShowTimer(true);
+	}, []);
+	return (
+		<div className="flex p-8 mt-4 w-full">
+			<p>Test</p>
+			{showTimer && <Countdown date={nextReset} renderer={renderer} />}
 		</div>
 	);
 }
