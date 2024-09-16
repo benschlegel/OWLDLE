075e6efd44519e6478110c57684f7c97d10ddf15
benschlegel <ben5.schlegel@gmail.com>
2024-09-16 19:09:54 +0200

    feat: pass backend data to win/loss screens

--- components/game-container/GameResult.tsx
+++ components/game-container/GameResult.tsx
@@ -1,19 +1,26 @@
 'use client';
 
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { GameStateContext } from '@/context/GameStateContext';
 import WinScreen from '@/components/game-container/win';
 import LossScreen from '@/components/game-container/lose';
+import type { ValidateResponse } from '@/types/server';
 
 export default function GameResult() {
 	const [gameState, setGameState] = useContext(GameStateContext);
+	const [validatedData, setValidatedData] = useState<ValidateResponse>();
+	useEffect(() => {
+		fetch('/api/validate?getplayer=true')
+			.then((response) => response.json())
+			.then((data) => setValidatedData(data));
+	}, []);
 
 	switch (gameState) {
 		case 'in-progress':
 			return <></>;
 		case 'won':
-			return <WinScreen />;
+			return <WinScreen nextReset={validatedData?.nextReset} />;
 		case 'lost':
-			return <LossScreen />;
+			return <LossScreen correctPlayer={validatedData?.correctPlayer} nextReset={validatedData?.nextReset} />;
 	}
 }
