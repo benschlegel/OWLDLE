05534b256e5c4549e18251aa99c4e22d3eac1f8a
benschlegel <ben5.schlegel@gmail.com>
2024-09-16 23:01:45 +0200

    feat: add copy result button

--- components/game-container/GameResult.tsx
+++ components/game-container/GameResult.tsx
@@ -1,16 +1,38 @@
 'use client';
 
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useMemo, useState } from 'react';
 import { GameStateContext } from '@/context/GameStateContext';
 import WinScreen from '@/components/game-container/win';
 import LossScreen from '@/components/game-container/lose';
 import type { ValidateResponse } from '@/types/server';
 import { useToast } from '@/hooks/use-toast';
+import type { RowData } from '@/components/game-container/GameContainer';
+import { Button } from '@/components/ui/button';
+import { CopyIcon } from 'lucide-react';
+import { formatResult } from '@/lib/client';
+import { GAME_CONFIG } from '@/lib/config';
 
-export default function GameResult() {
+type Props = {
+	results: RowData[];
+};
+
+export default function GameResult({ results }: Props) {
 	const [gameState, setGameState] = useContext(GameStateContext);
 	const [validatedData, setValidatedData] = useState<ValidateResponse>();
 	const { toast } = useToast();
+
+	// Memoize clipboard content
+	const formattedResult = useMemo(() => {
+		const guesses = results.map((row) => row.guessResult);
+		const formatted = formatResult({
+			gameName: GAME_CONFIG.gameName,
+			gameIteration: 1,
+			guesses: guesses,
+			maxGuesses: GAME_CONFIG.maxGuesses,
+			siteUrlShorthand: GAME_CONFIG.siteUrlShorthand,
+		});
+		return formatted;
+	}, [results]);
 
 	useEffect(() => {
 		fetch('/api/validate?getplayer=true')
@@ -25,12 +47,11 @@ export default function GameResult() {
 	}, [toast]);
 
 	// Win/LossScreen automatically reset game state if timer hits 0/next game starts
-
 	switch (gameState) {
 		case 'in-progress':
 			return <></>;
 		case 'won':
-			return <WinScreen nextReset={validatedData?.nextReset} />;
+			return <WinScreen nextReset={validatedData?.nextReset} formattedResult={formattedResult} />;
 		case 'lost':
 			return <LossScreen correctPlayer={validatedData?.correctPlayer} nextReset={validatedData?.nextReset} />;
 	}
