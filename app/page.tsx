933c6c9e6b3dacab6d1043e62688bda3d6360dfe
benschlegel <ben5.schlegel@gmail.com>
2024-09-16 14:59:50 +0200

    style: fix global padding + scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Toaster } from '@/components/ui/toaster';
 
 export default function Home() {
 	return (
-		<div className="px-4 py-8 sm:px-8 w-full h-full flex justify-center items-center">
+		<div className="px-4 pt-8 sm:px-8 w-full h-full flex justify-center items-center">
 			<main className="w-[32rem]">
 				<Header />
 				<Game />
