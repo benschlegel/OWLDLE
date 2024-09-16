8af67339fbba394f66dca16c53f5da644636168a
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-16 13:13:28 +0200

    feat: ensure that max guesses are respected

--- components/landing-page/game.tsx
+++ components/landing-page/game.tsx
@@ -3,34 +3,29 @@ import GameContainer, { type RowData } from '@/components/game-container/GameCon
 import PlayerSearch from '@/components/game-container/search';
 import { GuessContext } from '@/context/GuessContext';
 import { type FormattedPlayer, PLAYERS } from '@/data/players/formattedPlayers';
+import { GAME_CONFIG } from '@/lib/config';
 import type { GuessResponse } from '@/types/server';
 import { useContext, useEffect, useState } from 'react';
 
-const DEFAULT_GUESSES: RowData[] = [
-	{
-		player: PLAYERS[93],
-		guessResult: { isCountryCorrect: true, isNameCorrect: false, isRegionCorrect: true, isRoleCorrect: false, isTeamCorrect: false },
-	},
-];
-
 export default function Game() {
-	const [playerGuesses, setPlayerGuesses] = useContext(GuessContext);
+	const [playerGuesses, _] = useContext(GuessContext);
 	const [evaluatedGuesses, setEvaluatedGuesses] = useState<RowData[]>([]);
 
 	// Evaluate guess every time new guess comes in from GuessContext, merge and set new evaluated guesses
 	useEffect(() => {
-		const latestGuess: FormattedPlayer = playerGuesses[playerGuesses.length - 1];
-		if (latestGuess !== undefined) {
-			console.log('Guess: ', latestGuess);
-			fetch('/api/validate', {
-				method: 'POST',
-				body: JSON.stringify(latestGuess),
-			}).then(async (res) => {
-				const guessRespones: GuessResponse = await res.json();
-				const newRow: RowData = { guessResult: guessRespones, player: latestGuess };
-				console.log('new row: ', newRow);
-				setEvaluatedGuesses((prevEvaluatedGuesses) => [...prevEvaluatedGuesses, newRow]);
-			});
+		// Ensure that the max guesses are respected
+		if (playerGuesses.length <= GAME_CONFIG.maxGuesses) {
+			const latestGuess: FormattedPlayer = playerGuesses[playerGuesses.length - 1];
+			if (latestGuess !== undefined) {
+				fetch('/api/validate', {
+					method: 'POST',
+					body: JSON.stringify(latestGuess),
+				}).then(async (res) => {
+					const guessRespones: GuessResponse = await res.json();
+					const newRow: RowData = { guessResult: guessRespones, player: latestGuess };
+					setEvaluatedGuesses((prevEvaluatedGuesses) => [...prevEvaluatedGuesses, newRow]);
+				});
+			}
 		}
 	}, [playerGuesses]);
 	return (
