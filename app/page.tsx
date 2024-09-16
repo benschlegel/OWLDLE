e438d05646dc3dcbfd4fe83148127156e66ae54c
benschlegel <ben5.schlegel@gmail.com>
2024-09-16 14:33:35 +0200

    refactor: move Game out of Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Game from '@/components/landing-page/game';
 import Header from '@/components/landing-page/header';
 import { Toaster } from '@/components/ui/toaster';
 
@@ -6,6 +7,7 @@ export default function Home() {
 		<div className="px-4 py-8 sm:px-8 w-full h-full flex justify-center items-center">
 			<main className="w-[32rem]">
 				<Header />
+				<Game />
 			</main>
 			<Toaster />
 		</div>
