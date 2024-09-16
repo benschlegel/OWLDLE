ae89a9d75e54625275e4502a7086094f1b3ed774
benschlegel <ben5.schlegel@gmail.com>
2024-09-16 19:47:29 +0200

    feat: add countdown component

--- components/game-container/GameResult.tsx
+++ components/game-container/GameResult.tsx
@@ -5,15 +5,24 @@ import { GameStateContext } from '@/context/GameStateContext';
 import WinScreen from '@/components/game-container/win';
 import LossScreen from '@/components/game-container/lose';
 import type { ValidateResponse } from '@/types/server';
+import { useToast } from '@/hooks/use-toast';
 
 export default function GameResult() {
 	const [gameState, setGameState] = useContext(GameStateContext);
 	const [validatedData, setValidatedData] = useState<ValidateResponse>();
+	const { toast } = useToast();
+
 	useEffect(() => {
 		fetch('/api/validate?getplayer=true')
 			.then((response) => response.json())
+			.catch(() => {
+				toast({
+					title: "Can't reach server",
+					description: "Couldn't reach server",
+				});
+			})
 			.then((data) => setValidatedData(data));
-	}, []);
+	}, [toast]);
 
 	switch (gameState) {
 		case 'in-progress':
