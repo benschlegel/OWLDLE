e1a5fea14baa84e3c59dbab5b4aa31ffdefbf86a
benschlegel <ben5.schlegel@gmail.com>
2024-09-17 14:09:28 +0200

    feat: add github project link to socials

--- components/landing-page/socials.tsx
+++ components/landing-page/socials.tsx
@@ -2,9 +2,11 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Separator } from '@/components/ui/separator';
 import SocialsRow, { type SocialProps } from '@/components/ui/socials-row';
 import SocialsRowCopy from '@/components/ui/socials-row-copy';
-import { BotMessageSquareIcon, Github, Swords, TwitterIcon } from 'lucide-react';
+import { BotMessageSquareIcon, Github, GithubIcon, Swords, TwitterIcon } from 'lucide-react';
+import Link from 'next/link';
 
 const socials: SocialProps[] = [
 	{ socialName: 'Twitter', socialLink: 'https://x.com/scorer5_', socialValue: '@scorer5_', children: <TwitterIcon className="w-[1.1rem] h-[1.1rem] mb-1" /> },
@@ -24,20 +26,22 @@ const socials: SocialProps[] = [
 	},
 ];
 
+const projectGithubLink = 'https://github.com/benschlegel';
+
 export default function Socials() {
 	return (
-		<div className="flex justify-center items-center opacity-60 mb-[0.1rem]">
+		<div className="flex justify-center items-center opacity-60 mb-[0.2rem]">
 			<div className="text-sm font-medium leading-none">
 				Made with ❤️ by{' '}
 				<Popover>
 					<PopoverTrigger asChild>
-						<Button variant="link" className="p-0">
+						<Button variant="link" className="p-0 h-auto">
 							<code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] text-sm font-semibold" style={{ fontFamily: 'var(--font-geist-mono)' }}>
 								@scorer5
 							</code>
 						</Button>
 					</PopoverTrigger>
-					<PopoverContent className="w-80">
+					<PopoverContent className="w-80 mr-9 sm:mr-0">
 						<div className="grid gap-4">
 							<div className="space-y-2">
 								<h4 className="font-medium leading-none">My Socials</h4>
@@ -60,6 +64,20 @@ export default function Socials() {
 										</SocialsRow>
 									)
 								)}
+							</div>
+							<Separator />
+							<div className="text-sm text-muted-foreground">
+								You can also find this project on{' '}
+								<Link target="_blank" href={projectGithubLink} rel="noopener noreferrer" className="border-md">
+									<Button variant="link" className="p-0 h-auto" tabIndex={-1}>
+										<code
+											className="relative rounded bg-background px-[0.3rem] py-[0.2rem] text-sm font-semibold"
+											style={{ fontFamily: 'var(--font-geist-mono)' }}>
+											Github
+										</code>
+									</Button>
+								</Link>
+								!
 							</div>
 						</div>
 					</PopoverContent>
