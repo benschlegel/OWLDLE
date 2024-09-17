933c493a42b47e060a4aa46e1a2f1efc544a88a5
benschlegel <ben5.schlegel@gmail.com>
2024-09-17 13:47:28 +0200

    feat: implement social copy button

--- components/ui/socials-row-copy.tsx
+++ components/ui/socials-row-copy.tsx
@@ -1,15 +1,34 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
-import { CopyIcon } from 'lucide-react';
+import { CheckIcon, CopyIcon } from 'lucide-react';
 import Link from 'next/link';
-import type { PropsWithChildren } from 'react';
+import { useCallback, useState, type PropsWithChildren } from 'react';
 
 export type SocialProps = PropsWithChildren<{
 	socialName: string;
 	socialValue: string;
 }>;
+
+const copyDuration = 1500;
+
 export default function SocialsRowCopy({ socialName, socialValue, children }: SocialProps) {
+	const [isCopyActive, setIsCopyActive] = useState(false);
+
+	const handleCopy = useCallback(() => {
+		// Handle copy
+		setIsCopyActive(true);
+		navigator.clipboard.writeText(socialValue);
+
+		// Reset button state after timer
+		const timer = setTimeout(() => {
+			setIsCopyActive(false);
+		}, copyDuration);
+
+		// Cleanup timeout on unmount
+		return () => clearTimeout(timer);
+	}, [socialValue]);
+
 	return (
 		<div className="grid grid-cols-3 items-center gap-4">
 			<div className="flex flex-row gap-[0.28rem] justify-start items-center">
@@ -17,18 +36,17 @@ export default function SocialsRowCopy({ socialName, socialValue, children }: So
 				<Label htmlFor={`social-${socialName}`}>{socialName}</Label>
 			</div>
 			<div id={`social-${socialName}`} className="col-span-2 h-8 px-2 rounded-md border border-input bg-background justify-between flex items-center">
-				<Button
-					variant="link"
-					className="p-0 h-auto rounded-sm"
-					onClick={() => {
-						console.log('Clicked!');
-					}}>
+				<Button variant="link" className="p-0 h-auto rounded-sm" onClick={handleCopy}>
 					<code className="relative rounded  px-[0.3rem] py-[0.1rem] text-sm font-semibold" style={{ fontFamily: 'var(--font-geist-mono)' }}>
 						{socialValue}
 					</code>
 				</Button>
 				<Button variant="link" className="p-0 h-auto rounded-sm">
-					<CopyIcon className="w-[1.15rem] h-[1.15rem] opacity-70" />
+					{!isCopyActive ? (
+						<CopyIcon key="copy-button" className="w-[1.15rem] h-[1.15rem] opacity-70" onClick={handleCopy} />
+					) : (
+						<CheckIcon key="copy-button" className="w-[1.15rem] h-[1.15rem] opacity-70" />
+					)}
 				</Button>
 			</div>
 		</div>
