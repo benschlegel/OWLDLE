6a30da335c98872f4a736f73631f4627812f49ce
benschlegel <ben5.schlegel@gmail.com>
2024-09-17 23:24:04 +0200

    feat: add answer type for db

--- lib/databaseAccess.ts
+++ lib/databaseAccess.ts
@@ -1,5 +1,5 @@
 import { PLAYERS } from '@/data/players/formattedPlayers';
-import type { DbFormattedPlayers } from '@/types/database';
+import type { DbAnswer, DbFormattedPlayers } from '@/types/database';
 import { MongoClient, WithId } from 'mongodb';
 
 let useDevDatabase = false;
@@ -16,10 +16,12 @@ if (!uri) {
 const PROD_NAME = 'OWLEL';
 const DEV_NAME = 'OWLEL-dev';
 export const dbName = !useDevDatabase ? PROD_NAME : DEV_NAME;
-export const answerCollectionName = 'currentAnswer';
+export const currentAnswerCollectionName = 'currentAnswer';
+export const nextAnswerCollectionName = 'nextAnswer';
 export const playerCollectionName = 'players';
 const season1ID = 'season1';
-const answerID = 'today';
+const currentAnswerID = 'current';
+const nextAnswerID = 'next';
 
 // Use connect method to connect to the server
 const dbClient = new MongoClient(uri);
@@ -28,17 +30,31 @@ const database = dbClient.db(dbName);
 // Define collections
 
 const playerCollection = database.collection<DbFormattedPlayers>(playerCollectionName);
+const currentAnswerCollection = database.collection<DbFormattedPlayers>(currentAnswerCollectionName);
+const nextCollection = database.collection<DbFormattedPlayers>(nextAnswerCollectionName);
 
 export async function insertPlayer(): Promise<void> {
 	// const result = await playerCollection.insertMany(PLAYERS).then(() => console.log('Saved to db!'));
 }
 
+/**
+ * CAREFUL: deletes the entire player backlog from database
+ */
 export async function deleteAllPlayers() {
-	await playerCollection.deleteMany({});
+	return playerCollection.deleteMany({ _id: season1ID });
 }
 
+/**
+ * Insert all players from a season to the database backlog (if no object with the current season id exists, create it, otherwise, update)
+ */
 export async function insertAllPlayers() {
-	await playerCollection.updateOne({ _id: season1ID }, { $set: { _id: season1ID, players: PLAYERS } }, { upsert: true });
+	return playerCollection.updateOne({ _id: season1ID }, { $set: { _id: season1ID, players: PLAYERS } }, { upsert: true });
 }
-// const reminderCollection = database.collection<CreateReminder>(collectionNameReminders);
-// const metricCollection = database.collection<Userbase>(collectionNameMetrics);
+
+/**
+ * Sets the current game answer
+ * @param answer the answer that's currently correct
+ */
+export async function setCurrentAnswer(answer: DbAnswer) {
+	return currentAnswerCollection.updateOne({ _id: currentAnswerID }, { $set: { ...answer, _id: currentAnswerID } }, { upsert: true });
+}
