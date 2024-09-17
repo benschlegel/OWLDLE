c09b621f4091d0c79a298a91781ae19964bb191f
benschlegel <ben5.schlegel@gmail.com>
2024-09-17 16:09:30 +0200

    fix: fix plausible config (again)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 // import MillionLint from '@million/lint';
 import { withPlausibleProxy } from 'next-plausible';
 /** @type {import('next').NextConfig} */
-const nextConfig = withPlausibleProxy()({
+const nextConfig = withPlausibleProxy({ customDomain: 'https://plausible.global.bschlegel.com' })({
 	images: {
 		remotePatterns: [
 			{
