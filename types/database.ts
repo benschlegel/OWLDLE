5d79f36f15e6ea98d8feadccb845a2fe269e9ee2
benschlegel <ben5.schlegel@gmail.com>
2024-09-17 23:24:04 +0200

    feat: add answer type for db

--- types/database.ts
+++ types/database.ts
@@ -7,3 +7,22 @@ export interface DbFormattedPlayers {
 	_id: string;
 	players: FormattedPlayer[];
 }
+
+export interface DbAnswer {
+	/**
+	 * Should always be hard coded
+	 */
+	_id: string;
+	/**
+	 * The correct player
+	 */
+	player: FormattedPlayer;
+	/**
+	 * Time of next reset
+	 */
+	nextReset: Date;
+	/**
+	 * What iteration of the game this answer is for
+	 */
+	iteration: number;
+}
