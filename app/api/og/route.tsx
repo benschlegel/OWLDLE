520219530207f895fb20956b5d1cf1266cdc81d2
benschlegel <ben5.schlegel@gmail.com>
2024-09-17 15:07:19 +0200

    feat: basic og image working

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -2,11 +2,14 @@ import { ImageResponse } from 'next/og';
 // App router includes @vercel/og.
 // No need to install it.
 
+export const runtime = 'edge';
+
 export async function GET() {
+	const fontData = await fetch(new URL('/public/assets/OWLFontBold.woff', import.meta.url)).then((res) => res.arrayBuffer());
 	return new ImageResponse(
 		<div
 			style={{
-				fontSize: 40,
+				fontSize: 140,
 				color: '#f06216',
 				background: '#1a1a1e',
 				width: '100%',
@@ -14,13 +17,21 @@ export async function GET() {
 				padding: '50px 200px',
 				textAlign: 'center',
 				justifyContent: 'center',
+				fontFamily: 'OWLFontBold',
 				alignItems: 'center',
 			}}>
-			👋 Hello
+			OWLDLE
 		</div>,
 		{
 			width: 1200,
 			height: 630,
+			fonts: [
+				{
+					name: 'OWLFontBold',
+					data: fontData,
+					style: 'normal',
+				},
+			],
 		}
 	);
 }
