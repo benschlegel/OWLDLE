b004c67e5f047581f5259cde29269befdbe9d7d0
benschlegel <ben5.schlegel@gmail.com>
2024-09-17 19:50:02 +0200

    fix: attempt to polyfill aspect-ratio for old safari

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,6 +54,40 @@ const config: Config = {
 			},
 		},
 	},
-	plugins: [require('tailwindcss-animate')],
+	corePlugins: {
+		// Use custom aspect ratio plugin for better browser support
+		aspectRatio: false,
+	},
+	plugins: [
+		require('tailwindcss-animate'),
+		({ matchUtilities, theme /* … */ }) => {
+			// …
+			matchUtilities(
+				// https://gist.github.com/olets/9b833a33d01384eed1e9f1e106003a3b
+				{
+					aspect: (value) => ({
+						'@supports (aspect-ratio: 1 / 1)': {
+							aspectRatio: value,
+						},
+						'@supports not (aspect-ratio: 1 / 1)': {
+							// https://github.com/takamoso/postcss-aspect-ratio-polyfill
+
+							'&::before': {
+								content: '""',
+								float: 'left',
+								paddingTop: `calc(100% / (${value}))`,
+							},
+							'&::after': {
+								clear: 'left',
+								content: '""',
+								display: 'block',
+							},
+						},
+					}),
+				},
+				{ values: theme('aspectRatio') }
+			);
+		},
+	],
 };
 export default config;
