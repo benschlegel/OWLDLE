d823a708cc76a0469d646c79e6485c1b92681d18
benschlegel <ben5.schlegel@gmail.com>
2024-09-17 17:31:10 +0200

    refactor: use validated data in game result

--- components/game-container/GameResult.tsx
+++ components/game-container/GameResult.tsx
@@ -1,14 +1,11 @@
 'use client';
 
-import { useContext, useEffect, useMemo, useState } from 'react';
+import { useContext, useMemo } from 'react';
 import { GameStateContext } from '@/context/GameStateContext';
 import WinScreen from '@/components/game-container/win';
 import LossScreen from '@/components/game-container/lose';
 import type { ValidateResponse } from '@/types/server';
-import { useToast } from '@/hooks/use-toast';
 import type { RowData } from '@/components/game-container/GameContainer';
-import { Button } from '@/components/ui/button';
-import { CopyIcon } from 'lucide-react';
 import { formatResult } from '@/lib/client';
 import { GAME_CONFIG } from '@/lib/config';
 
@@ -20,8 +17,6 @@ type Props = {
 
 export default function GameResult({ results, iteration, validatedResponse }: Props) {
 	const [gameState, setGameState] = useContext(GameStateContext);
-	const [validatedData, setValidatedData] = useState<ValidateResponse>();
-	const { toast } = useToast();
 
 	// Memoize clipboard content
 	const formattedResult = useMemo(() => {
@@ -35,25 +30,13 @@ export default function GameResult({ results, iteration, validatedResponse }: Pr
 		return formatted;
 	}, [results, iteration]);
 
-	useEffect(() => {
-		fetch('/api/validate?getplayer=true')
-			.then((response) => response.json())
-			.catch(() => {
-				toast({
-					title: "Can't reach server",
-					description: "Couldn't reach server",
-				});
-			})
-			.then((data) => setValidatedData(data));
-	}, [toast]);
-
 	// Win/LossScreen automatically reset game state if timer hits 0/next game starts
 	switch (gameState) {
 		case 'in-progress':
 			return <></>;
 		case 'won':
-			return <WinScreen nextReset={validatedData?.nextReset} formattedResult={formattedResult} />;
+			return <WinScreen nextReset={validatedResponse?.nextReset} formattedResult={formattedResult} />;
 		case 'lost':
-			return <LossScreen correctPlayer={validatedData?.correctPlayer.name} formattedResult={formattedResult} nextReset={validatedData?.nextReset} />;
+			return <LossScreen correctPlayer={validatedResponse?.correctPlayer.name} formattedResult={formattedResult} nextReset={validatedResponse?.nextReset} />;
 	}
 }
--- components/landing-page/game.tsx
+++ components/landing-page/game.tsx
@@ -1,23 +1,11 @@
 'use client';
-import GameContainer, { type RowData } from '@/components/game-container/GameContainer';
+import GameContainer from '@/components/game-container/GameContainer';
 import GameResult from '@/components/game-container/GameResult';
 import PlayerSearch from '@/components/game-container/search';
 import SearchDialog from '@/components/game-container/search-dialog';
-import { GameStateContext } from '@/context/GameStateContext';
-import { GuessContext } from '@/context/GuessContext';
-import type { FormattedPlayer } from '@/data/players/formattedPlayers';
 import useGameState from '@/hooks/use-game-state';
-import { useToast } from '@/hooks/use-toast';
-import { GAME_CONFIG } from '@/lib/config';
-import type { GuessResponse } from '@/types/server';
-import { useCallback, useContext, useEffect, useState } from 'react';
 
 export default function Game() {
-	// const [playerGuesses, _] = useContext(GuessContext);
-	// const [evaluatedGuesses, setEvaluatedGuesses] = useState<RowData[]>([]);
-	// const [gameState, setGameState] = useContext(GameStateContext);
-	// const [currentGuess, setCurrentGuess] = useState<FormattedPlayer | undefined>(undefined);
-	// const { toast } = useToast();
 	const [evaluatedGuesses, gameState, validatedData] = useGameState();
 
 	return (
@@ -30,7 +18,7 @@ export default function Game() {
 					<SearchDialog />
 				</>
 			) : (
-				<GameResult results={evaluatedGuesses} />
+				<GameResult results={evaluatedGuesses} validatedResponse={validatedData} />
 			)}
 		</>
 	);
