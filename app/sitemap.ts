a399b73580fd6135d85f51e82e9477a60cd2fe70
benschlegel <ben5.schlegel@gmail.com>
2024-09-17 10:43:11 +0200

    feat: add sitemap generation

---
+++ app/sitemap.ts
@@ -0,0 +1,23 @@
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	const yesterday = new Date();
+	yesterday.setDate(yesterday.getDate() - 1);
+	yesterday.setHours(0);
+	yesterday.setMinutes(0);
+	yesterday.setSeconds(0);
+	return [
+		{
+			url: 'https://owls1le.bschlegel.com',
+			lastModified: yesterday,
+			changeFrequency: 'daily',
+			priority: 1,
+		},
+		// {
+		// 	url: 'https://acme.com/help',
+		// 	lastModified: new Date(),
+		// 	changeFrequency: 'monthly',
+		// 	priority: 0.8,
+		// },
+	];
+}
