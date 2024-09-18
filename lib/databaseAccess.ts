78b51030455d7fadf776c73e2576cd6b5f8c3bf0
benschlegel <ben5.schlegel@gmail.com>
2024-09-18 22:45:41 +0200

    feat: finish transaction logic

--- lib/databaseAccess.ts
+++ lib/databaseAccess.ts
@@ -43,6 +43,7 @@ const feedbackID = 'feedback';
 // Use connect method to connect to the server
 const dbClient = new MongoClient(uri);
 const database = dbClient.db(dbName);
+// TODO: dbClient.connect needed?
 
 // Define collections
 // TODO: add array with dataset as key and collections as value, dynamically generate
@@ -249,14 +250,14 @@ export async function addIteration(it: DbIteration, session?: ClientSession) {
 /**
  * Wrapper that handles going to the next iteration (if resetDate was reached)
  * Works as a transaction, either everything gets rolled over to next iteration or everything fails
- * @param nextReset hours until next reset
+ * @param nextResetHours hours until next reset
  */
 export async function goNextIteration(
-	nextReset: number = GAME_CONFIG.nextResetHours,
+	nextResetHours: number = GAME_CONFIG.nextResetHours,
 	dataset: DbDatasetID = 'OWL_season1',
 	backlogSize: number = GAME_CONFIG.backlogMaxSize
 ) {
-	// * Step 1: get current answer from db, error out if not working
+	// * Prep: get current answer from db, error out if not working
 	const currentAnswer = await getCurrentAnswer();
 	const nextAnswer = await getNextAnswer();
 	if (!currentAnswer || !nextAnswer) {
@@ -266,7 +267,7 @@ export async function goNextIteration(
 	// Prepare data
 	const nextIterationId = currentAnswer.iteration + 1;
 	const now = trimDate(new Date());
-	const nextResetDate = trimAndAddHours(now, GAME_CONFIG.nextResetHours);
+	const nextResetDate = trimAndAddHours(now, nextResetHours);
 	const nextAnswerData: DbAnswer = { iteration: nextIterationId, nextReset: nextResetDate, player: nextAnswer.player };
 
 	// Prepare transaction
@@ -316,6 +317,20 @@ export async function goNextIteration(
 					return Promise.reject(new Error('error while setting next iteration: could not get player from backlog'));
 				}
 
+				// * Generate and set new nextAnswer
+				try {
+					const nextAnswerIteration = nextIterationId + 1;
+					const nextAnswerReset = trimAndAddHours(nextResetDate, nextResetHours);
+					const answerRes = await setNextAnswer({ iteration: nextAnswerIteration, nextReset: nextAnswerReset, player: newPlayer }, dataset, session);
+					if (!answerRes.acknowledged) {
+						session.abortTransaction();
+						return Promise.reject(new Error('error while setting next iteration: failed to set new answer'));
+					}
+				} catch (e) {
+					session.abortTransaction();
+					return Promise.reject(new Error('error while setting next iteration: failed to set new answer'));
+				}
+
 				// * If new backlog is empty, regenerate it
 				if (newLength === 0) {
 					try {
@@ -335,7 +350,6 @@ export async function goNextIteration(
 	} finally {
 		await session.endSession();
 	}
-	// const nextIterationData: DbIteration = {dataset: dataset, }
 	// step 1: get current answer
 	// step 2: increment iteration
 	// step 3: set new current to old next
