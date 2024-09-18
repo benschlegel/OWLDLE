8d4326a794f135ead28e1e65831125dd0d7e6ffb
benschlegel <ben5.schlegel@gmail.com>
2024-09-18 22:57:59 +0200

    feat: improve db regen scripts

--- db-scripts/regenDb.ts
+++ db-scripts/regenDb.ts
@@ -15,14 +15,14 @@ if (env !== 'production') {
 	await insertAllPlayers();
 
 	// Insert answers
-	const currPlayer = formattedToDbPlayer(PLAYERS[0]);
-	await setCurrentAnswer({ player: currPlayer, iteration: 1, nextReset: new Date() });
-	const nextPlayer = formattedToDbPlayer(PLAYERS[3]);
-	await setNextAnswer({ player: nextPlayer, iteration: 2, nextReset: new Date() });
+	const randomPlayer = formattedToDbPlayer(PLAYERS[Math.floor(Math.random() * PLAYERS.length)]);
+	const randomPlayer2 = formattedToDbPlayer(PLAYERS[Math.floor(Math.random() * PLAYERS.length)]);
+	const curr = { player: randomPlayer, iteration: 1, nextReset: getFormattedMidnightTmrw() };
+	await setCurrentAnswer(curr);
+	await setNextAnswer({ player: randomPlayer2, iteration: 2, nextReset: getFormattedMidnightDayAfter() });
 
 	// Set first iteration
-	const player = PLAYERS[3];
-	await addIteration({ iteration: 0, dataset: 'OWL_season1', player: player, resetAt: new Date() });
+	await addIteration({ iteration: curr.iteration, dataset: 'OWL_season1', player: curr.player, resetAt: getFormattedMidnight() });
 
 	// Regen backlog
 	await generateBacklog();
@@ -34,3 +34,35 @@ if (env !== 'production') {
 
 console.info("Make sure to run this script with NODE_ENV!='production'");
 exit(-1);
+
+function getFormattedMidnight(): Date {
+	const midnight = new Date();
+	midnight.setHours(26);
+	midnight.setDate(midnight.getDate() - 1);
+	midnight.setMinutes(0);
+	midnight.setSeconds(0);
+	midnight.setMilliseconds(0);
+	return midnight;
+}
+
+function getFormattedMidnightTmrw(): Date {
+	const midnight = new Date();
+	midnight.setHours(26);
+	midnight.setMinutes(0);
+	midnight.setSeconds(0);
+	midnight.setMilliseconds(0);
+	return midnight;
+}
+
+function getFormattedMidnightDayAfter(): Date {
+	const midnight = new Date();
+	midnight.setHours(26);
+	midnight.setDate(midnight.getDate() + 1);
+	midnight.setMinutes(0);
+	midnight.setSeconds(0);
+	midnight.setMilliseconds(0);
+	return midnight;
+}
+
+// Before: curr: {id: 1, reset: 19., player: ArK}
+// Before: next: {id: 2, reset: 20., player: SoOn}
--- db-scripts/testNew.ts
+++ db-scripts/testNew.ts
@@ -1,11 +1,13 @@
-import { getCurrentIteration, popBacklog } from '@/lib/databaseAccess';
+import { GAME_CONFIG } from '@/lib/config';
+import { getCurrentIteration, goNextIteration, popBacklog } from '@/lib/databaseAccess';
 import { trimDate } from '@/lib/utils';
 import { exit } from 'node:process';
 
 console.time('test');
 
-const popped = await popBacklog();
-console.log('Popped item: ', popped);
+// const popped = await popBacklog();
+// console.log('Popped item: ', popped);
+await goNextIteration(GAME_CONFIG.nextResetHours, 'OWL_season1', GAME_CONFIG.backlogMaxSize);
 
 console.timeEnd('test');
 console.log('Finished.');
