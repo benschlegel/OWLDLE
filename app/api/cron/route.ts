fc21286b3ab850cf8cf0dde3bd7beeaecf20a964
benschlegel <ben5.schlegel@gmail.com>
2024-09-18 23:30:48 +0200

    feat: add iteration rollover to cron endpoint

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,4 +1,6 @@
 import { GET as RouteGet } from '@/app/api/validate/route';
+import { GAME_CONFIG } from '@/lib/config';
+import { goNextIteration } from '@/lib/databaseAccess';
 import type { NextRequest } from 'next/server';
 
 export const runtime = 'nodejs';
@@ -10,7 +12,13 @@ export async function GET(request: Request) {
 		return new Response('Unauthorized', { status: 401, statusText: 'Unauthorized' });
 	}
 
-	// TODO: Do db stuff
-	const reset = (await (await RouteGet({ ip: 'fake.ip.abc.de' } as unknown as NextRequest)).json()) as Date;
-	return new Response(`Next reset: ${JSON.stringify(reset)}`);
+	// Request authorized, safely run code starting here
+	// *
+	try {
+		await goNextIteration(GAME_CONFIG.nextResetHours, 'OWL_season1', GAME_CONFIG.backlogMaxSize);
+		const reset = (await (await RouteGet({ ip: 'fake.ip.abc.de' } as unknown as NextRequest)).json()) as Date;
+		return new Response('Successfully set next iteration and re-fetched on server', { status: 200 });
+	} catch (e) {
+		return new Response('Failed to set next iteration', { status: 500, statusText: 'Unauthorized' });
+	}
 }
