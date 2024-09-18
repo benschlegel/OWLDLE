b289f254225fe6af3b2e9c74b6783e1dac65b4b8
benschlegel <ben5.schlegel@gmail.com>
2024-09-19 00:12:34 +0200

    feat: automatic re-fetching after reset date working

---
+++ db-scripts/setCurrentDate.ts
@@ -0,0 +1,19 @@
+import { PLAYERS } from '@/data/players/formattedPlayers';
+import { setCurrentAnswer } from '@/lib/databaseAccess';
+import { formattedToDbPlayer } from '@/lib/databaseHelpers';
+import { exit } from 'node:process';
+
+const offsetMinutes = 5;
+
+console.time('setCurrent');
+const player = formattedToDbPlayer(PLAYERS[5]);
+const offsetDate = new Date();
+offsetDate.setMinutes(offsetDate.getMinutes() + offsetMinutes);
+offsetDate.setSeconds(0);
+offsetDate.setMilliseconds(0);
+// await setCurrentAnswer({ player: player, iteration: 2, nextReset: new Date() });
+const abc = await setCurrentAnswer({ iteration: 8, nextReset: offsetDate, player: player });
+console.log('Abc: ', abc);
+console.timeEnd('setCurrent');
+console.log('Finished.');
+exit(0);
