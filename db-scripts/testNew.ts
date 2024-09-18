081edae9d94fc9f5dea495852d5f80e82f439e0c
benschlegel <ben5.schlegel@gmail.com>
2024-09-18 22:13:32 +0200

    feat: add pop db functions to backlog

--- db-scripts/testNew.ts
+++ db-scripts/testNew.ts
@@ -1,11 +1,11 @@
-import { getCurrentIteration } from '@/lib/databaseAccess';
+import { getCurrentIteration, popBacklog } from '@/lib/databaseAccess';
 import { trimDate } from '@/lib/utils';
 import { exit } from 'node:process';
 
 console.time('test');
 
-const time = trimDate(new Date());
-console.log('Trimmed: ', time.toTimeString());
+const popped = await popBacklog();
+console.log('Popped item: ', popped);
 
 console.timeEnd('test');
 console.log('Finished.');
