b93549baa5452ac7019c68c676847efb399610e8
benschlegel <ben5.schlegel@gmail.com>
2024-09-18 12:51:07 +0200

    refactor: strongly type static db keys

--- types/database.ts
+++ types/database.ts
@@ -1,12 +1,15 @@
 import type { FormattedPlayer } from '@/data/players/formattedPlayers';
 
-export type DbSeasons = 'season1' | 'season2';
+export type DbSeasons = 'season1' | 'season2' | 'season3';
+export type DbAnswerPrefix = 'current' | 'next';
+
+export type AnswerKey = `${DbAnswerPrefix}_${DbSeasons}`;
 
 export interface DbFormattedPlayers {
 	/**
 	 * which season players are from
 	 */
-	_id: string;
+	_id: DbSeasons;
 	players: DbPlayer[];
 }
 
@@ -14,7 +17,7 @@ export interface DbAnswerFull {
 	/**
 	 * Should always be hard coded
 	 */
-	_id: string;
+	_id: AnswerKey;
 	/**
 	 * The correct player
 	 */
