2bc45817b5744da4daf646ceb90e49d814162944
benschlegel <ben5.schlegel@gmail.com>
2024-09-19 20:28:43 +0200

    feat: ensure unique on db backlog reroll

--- db-scripts/testNew.ts
+++ db-scripts/testNew.ts
@@ -1,8 +1,12 @@
 import { PLAYERS } from '@/data/players/formattedPlayers';
 import { GAME_CONFIG } from '@/lib/config';
 import {
+	generateBacklog,
 	getAllAnswers,
+	getBacklog,
+	getCurrentAnswer,
 	getCurrentIteration,
+	getNextAnswer,
 	getUniqueRandomPlayer,
 	goNextIteration,
 	popBacklog,
@@ -19,15 +23,8 @@ console.time('test');
 // const popped = await popBacklog();
 // console.log('Popped item: ', popped);
 try {
-	// const formattedPlayer = formattedToDbPlayer(PLAYERS[3]);
-	// await setPartialAnswer('current', formattedPlayer);
-	// await rerollAnswer('current');
-	const player = await getUniqueRandomPlayer('OWL_season1');
-	if (player) {
-		await rerollAnswer('current');
-	}
-	// const answers = await getAllAnswers('OWL_season1');
-	// console.log('Answers: ', answers);
+	const answers = await getAllAnswers('OWL_season1');
+	console.log('Answers: ', answers);
 	// await goNextIteration(GAME_CONFIG.nextResetHours, 'OWL_season1', GAME_CONFIG.backlogMaxSize);
 } catch (e) {
 	console.error('Failed to roll over iteration: ', e);
