4e8bb2d32d27291be936f84b510d4136ab274219
benschlegel <ben5.schlegel@gmail.com>
2024-09-21 16:55:12 +0200

    feat: save help screen state in localStorage

--- components/game-container/HelpDialog.tsx
+++ components/game-container/HelpDialog.tsx
@@ -1,11 +1,27 @@
 'use client';
-import FeedbackContent from '@/components/game-container/feedback-content';
 import HelpContent from '@/components/game-container/HelpContent';
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
-import { useState, type PropsWithChildren } from 'react';
+import { type PropsWithChildren, type SetStateAction, useCallback, useEffect, useState } from 'react';
+
+const localStorageKey = 'sawHelp';
 
 export function HelpDialog({ children }: PropsWithChildren) {
-	const [open, setOpen] = useState(false);
+	// Convoluted way to check to get value from localstorage and use it as default for the state (nextjs quirk with client components)
+	let localStorageVal: string | null = '';
+	if (typeof window !== 'undefined') {
+		localStorageVal = localStorage.getItem(localStorageKey);
+	}
+	let defaultVal = true;
+	if (localStorageVal && localStorageVal === 'false') {
+		defaultVal = false;
+	}
+	const [open, setOpen] = useState(defaultVal);
+
+	// Keep localStorage value in sync with state
+	useEffect(() => {
+		localStorage.setItem(localStorageKey, `${open}`);
+	}, [open]);
+
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
 			<DialogTrigger asChild>{children}</DialogTrigger>
