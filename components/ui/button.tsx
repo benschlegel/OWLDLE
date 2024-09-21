702c2cb2965f0322774a62c9c42a1d6e91097969
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-21 12:19:35 +0200

    style: fix button default styling (light theme)

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
 	{
 		variants: {
 			variant: {
-				default: 'bg-primary-foreground/80 text-primary hover:bg-primary-foreground',
+				default: 'bg-primary-foreground/80 text-white hover:bg-primary-foreground',
 				destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
 				outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
 				secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
