8b79824b50bfe8641db6b2bc742d35201a149202
benschlegel <ben5.schlegel@gmail.com>
2024-09-21 22:07:47 +0200

    fix: fix router issue + add query param based dialogs

--- components/game-container/feedback-dialog.tsx
+++ components/game-container/feedback-dialog.tsx
@@ -1,14 +1,23 @@
 'use client';
 import FeedbackContent from '@/components/game-container/feedback-content';
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
-import { useState, type PropsWithChildren } from 'react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState, type PropsWithChildren } from 'react';
 
 type Props = {
-	showFeedbackParam?: string | string[] | undefined;
+	showFeedbackParam?: string | null;
 };
 
 export function FeedbackDialog({ children, showFeedbackParam }: PropsWithChildren<Props>) {
 	const [open, setOpen] = useState(showFeedbackParam === 'true');
+	const router = useRouter();
+
+	useEffect(() => {
+		if (open === false) {
+			router.replace('/');
+		}
+	}, [open, router]);
+
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
 			<DialogTrigger asChild>{children}</DialogTrigger>
