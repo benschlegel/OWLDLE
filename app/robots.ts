ee8b354ccea0f23a34dcf1f0c62ef975239182b4
benschlegel <ben5.schlegel@gmail.com>
2024-09-21 15:39:02 +0200

    chore: add region icons

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
 		rules: {
 			userAgent: '*',
 			allow: '/',
-			disallow: '/teams/',
+			disallow: ['/teams/', '/regions/'],
 		},
 		sitemap: 'https://owldle.bschlegel.com/sitemap.xml',
 	};
