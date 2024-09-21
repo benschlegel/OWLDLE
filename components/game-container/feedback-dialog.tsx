67c406c820863497c8be0f32924a427f17d95e35
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-21 12:36:19 +0200

    feat: add star rating component to feedback

--- components/game-container/feedback-dialog.tsx
+++ components/game-container/feedback-dialog.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import StarRating from '@/components/ui/rate-stars';
 import { Textarea } from '@/components/ui/textarea';
 import { MessageSquareTextIcon } from 'lucide-react';
 
@@ -15,24 +16,32 @@ export function FeedbackDialog() {
 					<span className="sr-only">Send feedback</span>
 				</Button>
 			</DialogTrigger>
-			<DialogContent className="sm:max-w-[425px]">
+			<DialogContent className="sm:max-w-[32rem]">
 				<DialogHeader>
-					<DialogTitle>Feedback</DialogTitle>
-					<DialogDescription>If you have any suggestions, encountered any bugs or want to leave just leave feedback, you can do so here!</DialogDescription>
+					<DialogTitle className="text-left">Feedback</DialogTitle>
+					<DialogDescription className="text-left">
+						If you have any suggestions, encountered any bugs or want to leave just leave feedback, you can do so here!
+					</DialogDescription>
 				</DialogHeader>
 				<div className="grid gap-5 py-4">
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="name" className="text-right">
-							Name (optional)
+					<div className="grid grid-cols-4 items-center gap-4 grid-flow-row">
+						<Label htmlFor="name" className="text-left w-full col-span-4">
+							Name <span className="opacity-60">(optional)</span>
 						</Label>
-						<Input id="name" defaultValue="" className="col-span-3" />
+						<Input id="name" defaultValue="" className="col-span-4 " />
 					</div>
 					<div className="grid grid-cols-4 items-start gap-4">
-						<Label htmlFor="feedback" className="text-right pt-2">
+						<Label htmlFor="feedback" className="text-left w-full col-span-4">
 							Feedback
 						</Label>
-						<Textarea id="feedback" defaultValue="" className="col-span-3 max-h-52 h-32" />
+						<Textarea id="feedback" defaultValue="" className="col-span-4 max-h-52 h-24 md:h-32" />
 					</div>
+				</div>
+				<div className="grid grid-cols-4 items-center gap-2 grid-flow-row">
+					<Label htmlFor="name" className="text-left w-full col-span-4">
+						Rating <span className="opacity-60">(optional)</span>
+					</Label>
+					<StarRating />
 				</div>
 				<DialogFooter>
 					<Button type="submit">Send feedback</Button>
