9f5c0fd404d06449534f931a4c5385df70dce771
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-21 14:47:47 +0200

    feat: fix toaster not showing on mobile + align toast top right

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -13,10 +13,7 @@ const ToastViewport = React.forwardRef<React.ElementRef<typeof ToastPrimitives.V
 	({ className, ...props }, ref) => (
 		<ToastPrimitives.Viewport
 			ref={ref}
-			className={cn(
-				'fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]',
-				className
-			)}
+			className={cn('fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:right-0  sm:flex-col md:max-w-[420px]', className)}
 			{...props}
 		/>
 	)
