f3eeda9223476cca5a282aca3a004bc8738ca2bc
Ben Schlegel <ben5.schlegel@gmail.com>
2024-09-21 22:16:36 +0200

    feat: wrap header in suspense

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Game from '@/components/landing-page/game';
 import Header from '@/components/landing-page/header';
 import Socials from '@/components/landing-page/socials';
 import { Toaster } from '@/components/ui/toaster';
+import { Suspense } from 'react';
 
 export default function Home() {
 	return (
@@ -9,7 +10,9 @@ export default function Home() {
 			<div className="px-2 pt-8 sm:px-4 lg:px-8 w-full h-full flex justify-center items-center">
 				<main className="w-[32rem]">
 					<Socials />
-					<Header />
+					<Suspense>
+						<Header />
+					</Suspense>
 					<Game />
 				</main>
 			</div>
