1b2522666eed044b4b0d96730afb25e49c3fdb57
benschlegel <ben5.schlegel@gmail.com>
2024-09-23 23:25:23 +0200

    refactor: use new site url everywhere

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,7 +8,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
 	yesterday.setSeconds(0);
 	return [
 		{
-			url: 'https://owldle.bschlegel.com',
+			url: 'https://www.owldle.com/',
 			lastModified: yesterday,
 			changeFrequency: 'daily',
 			priority: 1,
