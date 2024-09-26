9200f45479760fccb3062b7ad70bcf8da8d9d07c
benschlegel <ben5.schlegel@gmail.com>
2024-09-26 17:44:02 +0200

    feat: use searchParams in feedback dialog

--- components/game-container/feedback-dialog.tsx
+++ components/game-container/feedback-dialog.tsx
@@ -1,15 +1,13 @@
 'use client';
 import FeedbackContent from '@/components/game-container/feedback-content';
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState, type PropsWithChildren } from 'react';
 
-type Props = {
-	showFeedbackParam?: string | null;
-};
-
-export function FeedbackDialog({ children, showFeedbackParam }: PropsWithChildren<Props>) {
-	const [open, setOpen] = useState(showFeedbackParam === 'true');
+export function FeedbackDialog({ children }: PropsWithChildren) {
+	const searchParams = useSearchParams();
+	const showFeedback = searchParams.get('showFeedback');
+	const [open, setOpen] = useState(showFeedback === 'true');
 	const router = useRouter();
 
 	useEffect(() => {
