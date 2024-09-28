f3390d19901296d5e8d04f2d2bb65318a3692791
benschlegel <ben5.schlegel@gmail.com>
2024-09-28 14:55:50 +0200

    feat: add mobile tooltip to TeamLogo

--- components/game-container/TeamLogo.tsx
+++ components/game-container/TeamLogo.tsx
@@ -1,9 +1,10 @@
-
+'use client';
 import { TEAM_LOGOS_S1 } from '@/data/teams/logos';
 import Image from 'next/image';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { splitCapitalization } from '@/lib/client';
 import { cn } from '@/lib/utils';
+import { useState } from 'react';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
 	teamName?: string;
@@ -12,13 +13,14 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export default function TeamLogo({ teamName, className, useTabIndex, disableBorder = false }: Props) {
+	const [open, setOpen] = useState(false);
 	const team = TEAM_LOGOS_S1.find((t) => t.teamName === teamName);
 
 	if (!teamName || !team) return <></>;
 
 	return (
 		<TooltipProvider delayDuration={0}>
-			<Tooltip>
+			<Tooltip open={open}>
 				<TooltipTrigger asChild>
 					<div className="p-1">
 						<div
@@ -31,7 +33,17 @@ export default function TeamLogo({ teamName, className, useTabIndex, disableBord
 								paddingBottom: '100%', // This creates the 1:1 aspect ratio
 							}}
 							tabIndex={useTabIndex ? 0 : -1}>
-							<div className="absolute inset-0 flex justify-center items-center p-[0.35rem]">
+							<button
+								type="button"
+								className="absolute inset-0 flex justify-center items-center p-[0.35rem] cursor-default"
+								onClick={() => setOpen(!open)}
+								onMouseEnter={() => setOpen(true)}
+								onMouseLeave={() => setOpen(false)}
+								onTouchStart={() => setOpen(!open)}
+								onKeyDown={(e) => {
+									e.preventDefault();
+									e.key === 'Enter' && setOpen(!open);
+								}}>
 								<Image
 									src={team.imgUrl}
 									alt={`Logo for ${team.displayName}`}
@@ -41,7 +53,7 @@ export default function TeamLogo({ teamName, className, useTabIndex, disableBord
 									height={64}
 									className=" h-auto w-auto max-h-full max-w-full object-contain"
 								/>
-							</div>
+							</button>
 						</div>
 					</div>
 				</TooltipTrigger>
