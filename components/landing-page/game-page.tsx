621a1e0a334d20b822eff539fcbd961cfd7aaae7
benschlegel <ben5.schlegel@gmail.com>
2024-09-28 23:39:35 +0200

    refactor: use dynamic route as app entry point + different datasets

--- components/landing-page/game-page.tsx
+++ components/landing-page/game-page.tsx
@@ -3,13 +3,17 @@ import Header from '@/components/landing-page/header';
 import Socials from '@/components/landing-page/socials';
 import { Toaster } from '@/components/ui/toaster';
 
-export default function Home() {
+type Props = {
+	slug: string;
+};
+
+export default function GamePage({ slug }: Props) {
 	return (
 		<>
 			<div className="px-2 pt-8 sm:px-4 lg:px-8 w-full h-full flex justify-center items-center">
 				<main className="w-[32rem]">
 					<Socials />
-					<Header />
+					<Header slug={slug} />
 					<Game />
 				</main>
 			</div>
