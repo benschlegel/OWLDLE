2435967fe9c1a3f497bba786e8e13788a199ff3e
benschlegel <ben5.schlegel@gmail.com>
2024-09-30 12:45:17 +0200

    feat: improve datasets + extractPlayers type

--- data/players/players.ts
+++ data/players/players.ts
@@ -136,7 +136,7 @@ export const s1Players: Player[] = [
 	{ name: 'xQc', country: 'CA', role: 'Tank', team: 'DallasFuel' },
 ] as const;
 
-export const s2Players: GenericPlayer<'season2'>[] = [
+export const PLAYERS_S2: GenericPlayer<'season2'>[] = [
 	// Eastern teams
 	{ name: 'babybay', country: 'US', role: 'Damage', team: 'AtlantaReign' },
 	{ name: 'Erster', country: 'KR', role: 'Damage', team: 'AtlantaReign' },
