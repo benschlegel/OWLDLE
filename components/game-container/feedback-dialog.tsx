423a7651869fc614cd688c6befee8f7a18e1907f
Ben Schlegel <ben5.schlegel@gmail.com>
2024-10-03 20:51:57 +0200

    refactor: move search param logic into feedback-dialog

--- components/game-container/feedback-dialog.tsx
+++ components/game-container/feedback-dialog.tsx
@@ -1,29 +1,26 @@
 'use client';
 import FeedbackContent from '@/components/game-container/feedback-content';
+import { Button } from '@/components/ui/button';
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
-import type { Options } from 'nuqs';
-import { useCallback, useEffect, useState, type PropsWithChildren } from 'react';
+import { MessageSquareTextIcon } from 'lucide-react';
+import { parseAsBoolean, useQueryState } from 'nuqs';
 
-type Props = {
-	slug: string;
-	setOpen: <Shallow>(value: boolean | ((old: boolean) => boolean | null) | null, options?: Options<Shallow> | undefined) => Promise<URLSearchParams>;
-	open: boolean;
-};
-
-export function FeedbackDialog({ children, slug, open, setOpen }: PropsWithChildren<Props>) {
-	useEffect(() => {
-		if (open === false) {
-			handleClose();
-		}
-	}, [open]);
-
-	const handleClose = useCallback(() => {
-		setOpen(null);
-	}, [setOpen]);
-
+export function FeedbackDialog() {
+	const [open, setOpen] = useQueryState('showFeedback', parseAsBoolean.withDefault(false));
 	return (
-		<Dialog open={open} onOpenChange={setOpen}>
-			<DialogTrigger asChild>{children}</DialogTrigger>
+		<Dialog open={open} onOpenChange={(val) => (val === true ? setOpen(true) : setOpen(null))}>
+			<DialogTrigger asChild>
+				<Button
+					variant="ghost"
+					size="icon"
+					className="p-0"
+					onClick={() => {
+						setOpen(null);
+					}}>
+					<MessageSquareTextIcon className="h-[1.2rem] w-[1.2rem]" />
+					<span className="sr-only">Send feedback</span>
+				</Button>
+			</DialogTrigger>
 			<FeedbackContent setIsOpen={setOpen} />
 		</Dialog>
 	);
