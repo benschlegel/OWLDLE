aca4e6c27b313895288cc5cd90e33ddbc8611a35
benschlegel <ben5.schlegel@gmail.com>
2024-10-04 21:37:28 +0200

    fix: fix wrong divisions for teams s1-s3

--- data/teams/teams.ts
+++ data/teams/teams.ts
@@ -5,24 +5,11 @@ import type { Player } from '@/types/players';
 // * Team data
 
 // Keep types in array to check what region player is from
-export const EASTERN = ['DallasFuel', 'LosAngelesGladiators', 'LosAngelesValiant', 'SanFranciscoShock', 'SeoulDynasty', 'ShanghaiDragons'] as const;
-export const WESTERN = ['BostonUprising', 'FloridaMayhem', 'HoustonOutlaws', 'LondonSpitfire', 'NewYorkExcelsior', 'PhiladelphiaFusion'] as const;
+export const EASTERN = ['BostonUprising', 'FloridaMayhem', 'HoustonOutlaws', 'LondonSpitfire', 'NewYorkExcelsior', 'PhiladelphiaFusion'] as const;
+export const WESTERN = ['DallasFuel', 'LosAngelesGladiators', 'LosAngelesValiant', 'SanFranciscoShock', 'SeoulDynasty', 'ShanghaiDragons'] as const;
 
 // Season 2
 const EASTERN_S2 = [
-	'ChengduHunters',
-	'HangzhouSpark',
-	'GuangzhouCharge',
-	'VancouverTitans',
-	'DallasFuel',
-	'LosAngelesGladiators',
-	'LosAngelesValiant',
-	'SanFranciscoShock',
-	'SeoulDynasty',
-	'ShanghaiDragons',
-] as const;
-
-const WESTERN_S2 = [
 	'AtlantaReign',
 	'ParisEternal',
 	'TorontoDefiant',
@@ -33,6 +20,19 @@ const WESTERN_S2 = [
 	'LondonSpitfire',
 	'NewYorkExcelsior',
 	'PhiladelphiaFusion',
+] as const;
+
+const WESTERN_S2 = [
+	'ChengduHunters',
+	'HangzhouSpark',
+	'GuangzhouCharge',
+	'VancouverTitans',
+	'DallasFuel',
+	'LosAngelesGladiators',
+	'LosAngelesValiant',
+	'SanFranciscoShock',
+	'SeoulDynasty',
+	'ShanghaiDragons',
 ] as const;
 
 // Season 3 same teams/split as S2
