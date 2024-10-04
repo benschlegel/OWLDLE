468ae6b1a5e2cab7868555698bf18779061f3893
benschlegel <ben5.schlegel@gmail.com>
2024-10-04 12:52:49 +0200

    style: fix owldle logo header position

--- components/landing-page/header.tsx
+++ components/landing-page/header.tsx
@@ -17,17 +17,17 @@ export default function Header({ slug }: Props) {
 					<SeasonSelector slug={slug} />
 				</div>
 				<div className="mb-1 flex items-center">
-					<div className="absolute left-1/2 transform -translate-x-1/2 text-center">
-						<h1
-							className="sm:text-4xl text-3xl font-bold text-center"
-							style={{
-								fontFamily: 'var(--font-owl-bold), ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
-							}}>
-							<span className="text-primary-foreground">OWL</span>
-							DLE
-						</h1>
-					</div>
+					{/* <div className="absolute left-1/2 transform -translate-x-1/2 text-center"> */}
+					<h1
+						className="sm:text-4xl text-3xl font-bold text-center sm:ml-[-1rem]"
+						style={{
+							fontFamily: 'var(--font-owl-bold), ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
+						}}>
+						<span className="text-primary-foreground">OWL</span>
+						DLE
+					</h1>
 				</div>
+				{/* </div> */}
 				<div className="flex gap-1">
 					<FeedbackDialog />
 					<ModeToggle />
