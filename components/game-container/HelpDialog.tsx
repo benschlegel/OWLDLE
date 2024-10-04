48e10aa0574db05ae9f2696bbbb58d53b1439de8
benschlegel <ben5.schlegel@gmail.com>
2024-10-04 10:35:08 +0200

    feat: add help dialog hotkey

--- components/game-container/HelpDialog.tsx
+++ components/game-container/HelpDialog.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 import { CircleHelpIcon } from 'lucide-react';
 import { parseAsBoolean, useQueryState } from 'nuqs';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 
 const localStorageKey = 'sawHelp';
 
@@ -39,8 +39,26 @@ export function HelpDialog() {
 			localStorage.setItem(localStorageKey, `${open}`);
 		}
 	}, [open]);
+
+	const toggleDialogOpen = useCallback(() => {
+		setOpen(open === true ? null : true);
+	}, [open, setOpen]);
+
+	useEffect(() => {
+		const down = (e: KeyboardEvent) => {
+			// Focus search on ctrl + y
+			if (e.key === 'e' && (e.metaKey || e.ctrlKey)) {
+				e.preventDefault();
+				toggleDialogOpen();
+			}
+		};
+
+		document.addEventListener('keydown', down);
+		return () => document.removeEventListener('keydown', down);
+	}, [toggleDialogOpen]);
+
 	return (
-		<Dialog open={open} onOpenChange={(val) => (val === true ? setOpen(true) : setOpen(null))} aria-describedby="Tutorial on how to play the game">
+		<Dialog open={open} onOpenChange={toggleDialogOpen} aria-describedby="Tutorial on how to play the game">
 			<DialogTrigger asChild>{TriggerButton}</DialogTrigger>
 			<HelpContent setIsOpen={setOpen} />
 		</Dialog>
