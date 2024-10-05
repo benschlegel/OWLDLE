2386787831bbcf54472349d5766fa9c1b976c94a
benschlegel <ben5.schlegel@gmail.com>
2024-10-05 21:52:41 +0200

    style: add sections to statistics

--- app/[stats]/PlayerStats.tsx
+++ app/[stats]/PlayerStats.tsx
@@ -1,7 +1,12 @@
 'use client';
 import Footer from '@/app/[stats]/Footer';
 import type { StatsRes } from '@/app/[stats]/types';
-import React, { useCallback, useEffect, useState } from 'react';
+import type React from 'react';
+import { useCallback, useEffect, useState } from 'react';
+
+const customFontStyle: React.CSSProperties = {
+	fontFamily: 'var(--font-owl-bold), ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
+};
 
 export default function GameStats() {
 	const [totalGames, setTotalGames] = useState<StatsRes>();
@@ -19,20 +24,31 @@ export default function GameStats() {
 	}, [getTotalGames]);
 	return (
 		<div className="flex-col flex w-full h-full">
-			<div className="px-14 py-10 flex-1 relative">
+			<div className="px-12 py-10 flex-1 relative">
 				<div>
-					<h1
-						className="sm:text-4xl text-3xl font-bold sm:ml-[-1rem]"
-						style={{
-							fontFamily: 'var(--font-owl-bold), ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
-						}}>
-						<span className="text-primary-foreground text-6xl">Statistics</span>
+					<h1 className="text-primary-foreground text-6xl font-bold" style={customFontStyle}>
+						Statistics
 					</h1>
 				</div>
-				{/* <div className="absolute top-0 right-0 border-l-2 border-b-2 rounded-lg h-[170px] w-[500px]">Test</div> */}
-				<div>
-					<p>Test</p>
+				{/* Sub-sections */}
+				<div className="mt-10" />
+				<div className="flex flex-row">
+					<div className="flex flex-1">
+						<div className="w-full flex justify-center">
+							<h3 className="text-3xl font-bold text-center" style={customFontStyle}>
+								Games per season
+							</h3>
+						</div>
+					</div>
+					<div className="flex flex-1">
+						<div className="w-full flex justify-center">
+							<h3 className="text-3xl font-bold text-center" style={customFontStyle}>
+								Win percentage
+							</h3>
+						</div>
+					</div>
 				</div>
+				{/* <div className="absolute top-0 right-0 border-l-2 border-b-2 rounded-lg h-[170px] w-[500px]">Test</div> */}
 			</div>
 			<Footer />
 		</div>
