d87b08a789c80ce7af40dbdf268700a5c4a9cf83
benschlegel <ben5.schlegel@gmail.com>
2024-10-06 00:31:36 +0200

    refactor: extract PlayerStats component to make swapping easier

--- app/[stats]/PlayerStats.tsx
+++ app/[stats]/PlayerStats.tsx
@@ -1,15 +1,9 @@
 'use client';
-import Footer from '@/app/[stats]/Footer';
-import SeasonStats from '@/app/[stats]/SeasonStats';
+import GlobalStatsScreen from '@/app/[stats]/GlobalStats';
 import type { StatsRes } from '@/app/[stats]/types';
-import WinPercentStats from '@/app/[stats]/WinPercentStats';
 import type { Dataset } from '@/data/datasets';
 import type React from 'react';
 import { useCallback, useEffect, useState } from 'react';
-
-const customFontStyle: React.CSSProperties = {
-	fontFamily: 'var(--font-owl-bold), ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
-};
 
 export default function GameStats() {
 	const [totalGames, setTotalGames] = useState<StatsRes>();
@@ -40,52 +34,6 @@ export default function GameStats() {
 	}, [getTotalGames]);
 
 	if (!totalGames) return <></>;
-	const avgWin = totalGames.winPercent.reduce((r, c) => r + c.winPercentage, 0) / totalGames.winPercent.length;
 
-	return (
-		<div className="flex-col flex w-full h-full">
-			<div className="px-12 pt-10 flex-1 relative">
-				<div>
-					<h1 className="text-primary-foreground text-6xl font-bold" style={customFontStyle}>
-						Statistics
-					</h1>
-				</div>
-				{/* Sub-sections */}
-				<div className="mb-8" />
-				<div className="flex flex-row gap-10">
-					<div className="flex flex-1 flex-col">
-						<div className="w-full flex justify-center">
-							<h3 className="text-3xl font-bold text-center mb-4" style={customFontStyle}>
-								Games per season
-							</h3>
-						</div>
-						<SeasonStats data={totalGames?.seasons} />
-						<div className="flex gap-2 items-end mt-4">
-							<h4 className="text-lg font-bold" style={customFontStyle}>
-								Total games played:
-							</h4>
-							<h4 className="font-mono font-bold text-2xl opacity-80">{totalGames?.total.toLocaleString()}</h4>
-						</div>
-					</div>
-
-					<div className="flex flex-1 flex-col">
-						<div className="w-full flex justify-center">
-							<h3 className="text-3xl font-bold text-center mb-4" style={customFontStyle}>
-								Win percentage
-							</h3>
-						</div>
-						<WinPercentStats data={totalGames?.winPercent} />
-						<div className="flex gap-2 items-end mt-4">
-							<h4 className="text-lg font-bold" style={customFontStyle}>
-								Global win percentage:
-							</h4>
-							<h4 className="font-mono font-bold text-xl opacity-80">{avgWin.toFixed(2)}%</h4>
-						</div>
-					</div>
-				</div>
-				{/* <div className="absolute top-0 right-0 border-l-2 border-b-2 rounded-lg h-[170px] w-[500px]">Test</div> */}
-			</div>
-			<Footer />
-		</div>
-	);
+	return <GlobalStatsScreen stats={totalGames} />;
 }
