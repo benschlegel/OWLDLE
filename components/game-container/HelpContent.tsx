6fa79078f787eccd9ca1cfe6c3d141abd2f65cec
benschlegel <ben5.schlegel@gmail.com>
2024-10-07 13:56:28 +0200

    style: fix height for block quote in help for mobile

--- components/game-container/HelpContent.tsx
+++ components/game-container/HelpContent.tsx
@@ -76,7 +76,7 @@ export default function HelpContent({ setIsOpen }: Props) {
 			<ScrollArea type="scroll" className="h-[440px]">
 				<main className="h-full w-full flex flex-col gap-6 px-2 pb-2 text-wrap break-words ">
 					{/* Description section */}
-					<blockquote className="leading-7 tracking-wide opacity-90 border-l-[3px] pl-4 mt-1">
+					<blockquote className="sm:leading-7 tracking-wide opacity-90 border-l-[3px] pl-4 mt-1">
 						Guess the correct Overwatch League player within 8 attempts to win. After each guess, you will receive hints based on attributes like the player's
 						role, team, region and nationality to help you get closer to the right answer.
 					</blockquote>
