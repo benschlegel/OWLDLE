7da9b8b59e435dc56cba327346c41d55a8a4c111
benschlegel <ben5.schlegel@gmail.com>
2024-10-08 19:53:01 +0200

    perf: memoize header

--- components/landing-page/game-page.tsx
+++ components/landing-page/game-page.tsx
@@ -2,18 +2,20 @@ import Game from '@/components/landing-page/game';
 import Header from '@/components/landing-page/header';
 import Socials from '@/components/landing-page/socials';
 import { Toaster } from '@/components/ui/toaster';
+import React from 'react';
 
 type Props = {
 	slug: string;
 };
 
+const MemoizedHeader = React.memo(Header);
 export default function GamePage({ slug }: Props) {
 	return (
 		<>
 			<div className="px-2 pt-8 sm:px-4 lg:px-8 w-full h-full flex justify-center items-center">
 				<main className="w-[32rem]">
 					<Socials />
-					<Header slug={slug} />
+					<MemoizedHeader slug={slug} />
 					<Game slug={slug} />
 				</main>
 			</div>
