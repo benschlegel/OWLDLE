dd359ba1e606114fdc36960704450d224a0b63ed
Ben Schlegel <ben5.schlegel@gmail.com>
2024-10-11 15:05:06 +0200

    feat: save game state to localStorage

--- hooks/use-game-state.tsx
+++ hooks/use-game-state.tsx
@@ -2,8 +2,8 @@ import type { RowData } from '@/components/game-container/GameContainer';
 import { DatasetContext } from '@/context/DatasetContext';
 import { GameStateContext } from '@/context/GameStateContext';
 import { GuessContext } from '@/context/GuessContext';
-import { type Dataset, DEFAULT_DATASET, getDataset } from '@/data/datasets';
-import type { CombinedFormattedPlayer, FormattedPlayer } from '@/data/players/formattedPlayers';
+import { type Dataset, DATASETS, DEFAULT_DATASET, getDataset } from '@/data/datasets';
+import type { CombinedFormattedPlayer } from '@/data/players/formattedPlayers';
 import { GAME_CONFIG } from '@/lib/config';
 import { validateGuess } from '@/lib/server';
 import type { DbSaveData } from '@/types/database';
@@ -19,6 +19,8 @@ type Props = {
 	slug: string;
 };
 
+export const LOCAL_STORAGE_STATE_KEY = 'gameState';
+
 export default function useGameState({ slug }: Props) {
 	const [playerGuesses, setPlayerGuesses] = useContext(GuessContext);
 	const [gameState, setGameState] = useContext(GameStateContext);
@@ -30,7 +32,6 @@ export default function useGameState({ slug }: Props) {
 	const { data: validatedData } = useAnswerQuery(dataset.dataset);
 	const { data, isOld } = useEvaluatedGuesses(dataset.dataset);
 	const { evaluatedGuesses, setEvaluatedGuesses } = data;
-	// TODO: compare lastPlayer to validatedData
 
 	// Update dataset and reset guesses when slug cahnges (slug change updates dataset)
 	useEffect(() => {
@@ -39,18 +40,28 @@ export default function useGameState({ slug }: Props) {
 	}, [dataset, setDataset]);
 
 	const resetGuesses = useCallback(() => {
+		// * If evaluated guesses exist (read from localStorage), use them + update gameState
 		if (evaluatedGuesses.length > 0) {
-			setGameState('in-progress');
-			const guesses = evaluatedGuesses.map((e) => e.player);
+			if (typeof window !== 'undefined') {
+				const gameStateOld = localStorage.getItem(LOCAL_STORAGE_STATE_KEY);
+				if (gameStateOld) {
+					const parsedOldState = JSON.parse(gameStateOld) as SavedState;
+					setGameState(parsedOldState[dataset.dataset]);
+				} else {
+					const defaultValues = getDefaultGameState();
+					localStorage.setItem(LOCAL_STORAGE_STATE_KEY, JSON.stringify(defaultValues));
+					setGameState('in-progress');
+				}
+			}
 			isRollbackRef.current = true;
-			setPlayerGuesses(guesses);
 		} else {
+			// * Regular reset
 			setGameState('in-progress');
 			isRollbackRef.current = true;
 			setPlayerGuesses([]);
 			setEvaluatedGuesses([]);
 		}
-	}, [setPlayerGuesses, setGameState, setEvaluatedGuesses, evaluatedGuesses]);
+	}, [setPlayerGuesses, setGameState, setEvaluatedGuesses, evaluatedGuesses, dataset.dataset]);
 
 	// Evaluate guess every time new guess comes in from GuessContext, merge and set new evaluated guesses
 	useEffect(() => {
@@ -80,7 +91,7 @@ export default function useGameState({ slug }: Props) {
 		const currentGuess: CombinedFormattedPlayer = playerGuesses[playerGuesses.length - 1];
 
 		// Double-check that conditions are valid (should always be the case already)
-		if (validatedData !== undefined && gameState === 'in-progress') {
+		if (validatedData !== undefined && gameState === 'in-progress' && isRollbackRef.current === false) {
 			console.log(`Applying...`, currentGuess);
 			// Validate data
 			const correctPlayer = validatedData.correctPlayer;
@@ -90,6 +101,7 @@ export default function useGameState({ slug }: Props) {
 			const newRow: RowData = { guessResult: guessResult, player: currentGuess };
 			setEvaluatedGuesses((prevEvaluatedGuesses) => [...prevEvaluatedGuesses, newRow]);
 			const newData = [...evaluatedGuesses, newRow];
+			console.log('Curr guesses: ', newData.length);
 
 			// TODO: refactor game over logic in callback and use while reading old one
 			// Determine game state
@@ -99,6 +111,9 @@ export default function useGameState({ slug }: Props) {
 			} else if (newData.length === GAME_CONFIG.maxGuesses) {
 				result = 'lost';
 			}
+
+			// Save current state to localStorage
+			saveLocalStorageState(dataset.dataset, result);
 
 			// Save result and update game state
 			if (result !== 'in-progress') {
@@ -111,7 +126,32 @@ export default function useGameState({ slug }: Props) {
 			isRollbackRef.current = true;
 			setPlayerGuesses((old) => old.slice(0, -1));
 		}
-	}, [gameState, validatedData, playerGuesses, setGameState, evaluatedGuesses, setPlayerGuesses, saveGame, setEvaluatedGuesses]);
+	}, [gameState, validatedData, playerGuesses, setGameState, evaluatedGuesses, setPlayerGuesses, saveGame, setEvaluatedGuesses, dataset.dataset]);
 
 	return [evaluatedGuesses, gameState, validatedData, isOld] as const;
+}
+
+type SavedState = { [key: string]: GameState };
+function getDefaultGameState() {
+	const mappedState: SavedState = {};
+	for (const dataset of DATASETS) {
+		mappedState[dataset] = 'in-progress';
+	}
+	return mappedState;
+}
+
+function saveLocalStorageState(dataset: Dataset, state: GameState) {
+	if (typeof window !== 'undefined') {
+		const old = localStorage.getItem(LOCAL_STORAGE_STATE_KEY);
+		if (old) {
+			// Update old state if it exists.
+			const parsed = JSON.parse(old);
+			parsed[dataset] = state;
+			localStorage.setItem(LOCAL_STORAGE_STATE_KEY, JSON.stringify(parsed));
+		} else {
+			// If entry doesn't exist yet, add it
+			const defaultValues = getDefaultGameState();
+			localStorage.setItem(LOCAL_STORAGE_STATE_KEY, JSON.stringify(defaultValues));
+		}
+	}
 }
