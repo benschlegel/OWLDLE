3b6e7e0bc56be9df0fe96d662aa89ccccda57d66
Ben Schlegel <ben5.schlegel@gmail.com>
2024-10-11 11:16:02 +0200

    feat: add basic persistence integration for react query (+ dynamic stale time)

--- context/ReactQueryProvider.tsx
+++ context/ReactQueryProvider.tsx
@@ -1,12 +1,45 @@
 'use client';
 
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { useState } from 'react';
+import { type Persister, PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import { QueryClient } from '@tanstack/react-query';
+import { useState, useEffect } from 'react';
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
-	const [queryClient] = useState(() => new QueryClient());
+	// Create QueryClient with default options
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						gcTime: 1000 * 60 * 60 * 24, // 24 hours
+					},
+				},
+			})
+	);
 
-	return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+	// Use state for the persister
+	const [persister, setPersister] = useState<Persister | null>(null);
+
+	// Only create the persister on the client (after first render)
+	useEffect(() => {
+		// Ensure we are in the browser
+		if (typeof window !== 'undefined') {
+			const storagePersister = createSyncStoragePersister({
+				storage: window.localStorage,
+			});
+			setPersister(storagePersister);
+		}
+	}, []); // Runs once after the first render
+
+	// Render only when the persister is available
+	if (!persister) return null;
+
+	return (
+		<PersistQueryClientProvider persistOptions={{ persister }} client={queryClient}>
+			{children}
+		</PersistQueryClientProvider>
+	);
 };
 
 export default ReactQueryProvider;
