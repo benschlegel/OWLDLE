71ce6e0494ff227b61de162b82804d40e3c67d1e
Ben Schlegel <ben5.schlegel@gmail.com>
2025-03-03 21:46:17 +0100

    fix: fix next theme provider type issues

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import type { ThemeProviderProps } from 'next-themes/dist/types';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+// biome-ignore lint/suspicious/noExplicitAny: Importing the proper type keeps causing issues between pc and mac
+export function ThemeProvider({ children, ...props }: any) {
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
