8163977b836f093405277a48ef10bc720c6b613a
Ben Schlegel <ben5.schlegel@gmail.com>
2025-03-04 17:15:36 +0100

    feat: update page metadata (opengraph, title, etc)

--- app/owcs/page.tsx
+++ app/owcs/page.tsx
@@ -1,15 +1,24 @@
-import { DEFAULT_TITLE, metadata as prevMetadata } from '@/app/layout';
+import { DEFAULT_DESCRIPTION, DEFAULT_TITLE, OgConfig, metadata as prevMetadata } from '@/app/layout';
 import type { Metadata } from 'next';
 import { Suspense } from 'react';
 import SeasonPageWrapper from '@/app/owcs/PageWrapper';
+import { GAME_CONFIG } from '@/lib/config';
 
 const DEFAULT_NAME = 'OWCS';
 
 export async function generateMetadata({ searchParams }: { searchParams: Promise<{ season?: string }> }): Promise<Metadata> {
 	const { season } = await searchParams;
+	// Query param only contains number, e.g. "6" -> season6
+
+	// Format dataset (e.g. "season1" to "Season 1")
+	const formattedTitle = `${DEFAULT_TITLE} - OWCS Edition`;
+	const formattedDescritpion = 'Guess the correct Overwatch Champion Series player within 8 tries to win.';
+	const openGraphTitle = formattedTitle;
+
+	const ogImagePath = `/open-graph/owcs-s2.png`;
 	return {
 		...prevMetadata,
-		title: `${DEFAULT_TITLE} - OWCS`,
+		title: formattedTitle,
 		alternates: {
 			canonical: '/owcs',
 		},
@@ -26,6 +35,50 @@ export async function generateMetadata({ searchParams }: { searchParams: Promise
 					href: '/owcs_favicon.ico',
 				},
 			],
+		},
+		openGraph: {
+			title: openGraphTitle,
+			description: formattedDescritpion,
+			url: `${GAME_CONFIG.siteUrl}/owcs`,
+			images: [
+				{
+					alt: openGraphTitle,
+					url: ogImagePath,
+					width: OgConfig.ogImageWidth,
+					height: OgConfig.ogImageHeight,
+					type: 'image/png',
+				},
+			],
+			type: 'website',
+			siteName: 'owldle',
+		},
+		twitter: {
+			title: openGraphTitle,
+			description: DEFAULT_DESCRIPTION,
+			site: GAME_CONFIG.siteUrl,
+			siteId: 'owldle',
+			images: [
+				{
+					url: `https://www.owldle.com${ogImagePath}`,
+					alt: formattedTitle,
+					width: OgConfig.ogImageWidth,
+					height: OgConfig.ogImageHeight,
+					type: 'image/png',
+				},
+			],
+			card: 'summary_large_image',
+		},
+		robots: {
+			index: false,
+			follow: true,
+			nocache: true,
+			googleBot: {
+				index: true,
+				follow: false,
+				noimageindex: true,
+				'max-video-preview': -1,
+				'max-snippet': -1,
+			},
 		},
 	};
 }
