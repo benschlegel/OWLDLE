bf1b19840357f15ba8cba493c08f88151268893d
Ben Schlegel <ben5.schlegel@gmail.com>
2026-03-25 16:18:52 +0100

    feat: replace old/inconsistent/laggy confetti with react-confetti + fix bugs with confetti firing when navigating after win

--- components/game-container/win.tsx
+++ components/game-container/win.tsx
@@ -3,14 +3,13 @@ import { SwitchableButton } from '@/components/ui/switchable-button';
 import { GameStateContext } from '@/context/GameStateContext';
 import { GuessContext } from '@/context/GuessContext';
 import { CheckIcon, CopyIcon } from 'lucide-react';
-import { useCallback, useContext, useEffect, useLayoutEffect, useState } from 'react';
+import { useContext, useLayoutEffect, useState } from 'react';
 import Countdown, { type CountdownRenderProps, zeroPad } from 'react-countdown';
-import Confetti from 'react-confetti';
-import { Button } from '@/components/ui/button';
 import { usePlausible } from 'next-plausible';
 import type { PlausibleEvents } from '@/types/plausible';
 import type { Dataset } from '@/data/datasets';
 import FooterText from '@/components/footer-text';
+import GameConfetti from '@/components/game-container/game-confetti';
 
 type Props = {
 	nextReset: Date;
@@ -22,70 +21,23 @@ type Props = {
 	isOldState: boolean;
 };
 
-const DEFAULT_CONFETTI_DURATION = 7500;
-const OLD_CONFETTI_DURATION = 500;
-
 export default function WinScreen({ nextReset, formattedResult, dataset, isOldState }: Partial<Props>) {
-	const [gameState, setGameState] = useContext(GameStateContext);
+	const [_gameState, setGameState] = useContext(GameStateContext);
 	const [_guesses, setGuesses] = useContext(GuessContext);
 	const [showTimer, setShowTimer] = useState(true);
-	const [showConfetti, setShowConfetti] = useState(true);
 	const plausible = usePlausible<PlausibleEvents>();
-	const confettiDuration = isOldState ? OLD_CONFETTI_DURATION : DEFAULT_CONFETTI_DURATION;
-
-	const [showButtonConfetti, setShowButtonConfetti] = useState(false);
-
-	useEffect(() => {
-		if (gameState === 'won') {
-			const element = document.getElementById('win');
-			if (element) {
-				element.scrollIntoView({ behavior: 'smooth' });
-			}
-		}
-	}, [gameState]);
 
 	// Fix hydration warning for mismatching countdown time
 	useLayoutEffect(() => {
 		setShowTimer(true);
 	}, []);
 
-	// Stops running confetti after 'confettiDuration' amount of ms
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			if (showConfetti) {
-				setShowConfetti(false);
-			}
-		}, confettiDuration);
-
-		// Cleanup timeout on unmount
-		return () => clearTimeout(timer);
-	}, [showConfetti, confettiDuration]);
-
-	const handleConfettiButton = useCallback(() => {
-		plausible('clickConfetti', { props: { state: 'won' } });
-		setShowConfetti(true);
-	}, [plausible]);
-
 	if (nextReset === undefined) return <></>;
 
 	return (
 		<div id="win" className="flex p-4 gap-1 justify-center items-center mt-4 w-full flex-col">
 			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-				<Button
-					className="p-0 py-8 text-4xl font-extrabold tracking-tight lg:text-5xl"
-					variant="ghost"
-					onClick={handleConfettiButton}
-					aria-label="Show confetti">
-					🎉
-				</Button>{' '}
-				You won!{' '}
-				<Button
-					className="p-0 py-8 text-4xl font-extrabold tracking-tight lg:text-5xl"
-					variant="ghost"
-					onClick={handleConfettiButton}
-					aria-label="Show confetti">
-					🎉
-				</Button>
+				🎉 You won! 🎉
 			</h1>
 			<div className="flex gap-2 items-center justify-center opacity-80">
 				<p>Time until next reset:</p>
@@ -108,35 +60,23 @@ export default function WinScreen({ nextReset, formattedResult, dataset, isOldSt
 				onClick={() => {
 					navigator.clipboard.writeText(formattedResult ?? '');
 					plausible('copyResult', { props: { state: 'won', dataset: dataset ?? 'season1' } });
-					setShowButtonConfetti(true);
 				}}
 				switchedContent={<SwitchedButtonContent />}>
 				<DefaultButtonContent />
 			</SwitchableButton>
 
-			<Confetti numberOfPieces={showConfetti ? 200 : 0} className="overflow-none w-full h-full z-50 fixed top-0 left-0" />
-			<Confetti
-				numberOfPieces={showButtonConfetti ? 20 : 0}
-				className="overflow-none w-full h-full z-50 fixed top-0 left-0"
-				onConfettiComplete={(confetti) => {
-					setShowButtonConfetti(false);
-					confetti?.reset();
-				}}
-				recycle={false}
-			/>
+			<GameConfetti isOldState={isOldState ?? false} />
 			<FooterText />
 		</div>
 	);
 }
 
-function renderer({ days, hours, minutes, seconds, milliseconds, completed }: CountdownRenderProps) {
+function renderer({ hours, minutes, seconds, completed }: CountdownRenderProps) {
 	if (completed) {
-		// Render a completed state
 		return <></>;
 	}
-	// Render a countdown
 	return (
-		<p className="gap-2 font-mono font-bold mt-[2px]">
+		<p className="gap-2 font-mono font-bold mt-0.5">
 			<span>{zeroPad(hours)}</span>:<span>{zeroPad(minutes)}</span>:<span>{zeroPad(seconds)}</span>
 		</p>
 	);
