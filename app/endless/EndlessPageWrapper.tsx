4880ecc872eba9f184fe93aeaeb917ede9e1077d
Ben Schlegel <ben5.schlegel@gmail.com>
2026-03-26 10:48:12 +0100

    feat: add endless badge

--- app/endless/EndlessPageWrapper.tsx
+++ app/endless/EndlessPageWrapper.tsx
@@ -8,6 +8,7 @@ import Socials from '@/components/landing-page/socials';
 import { useEndlessParams } from '@/hooks/use-endless-params';
 import { isOwcsDataset } from '@/data/datasets';
 import React from 'react';
+import { GamepadIcon } from 'lucide-react';
 
 const MemoizedSocials = React.memo(Socials);
 
@@ -20,8 +21,19 @@ export default function EndlessPageWrapper() {
 			<div className="sm:hidden block">
 				<MemoizedSocials />
 			</div>
-			<GameHeader topLabel="Endless Mode" modeLabel={modeLabel} seasonSelector={<EndlessSeasonSelector />} seasonTitle={<EndlessSeasonTitle />} />
+			<GameHeader topLabel={<EndlessHeaderBadge />} modeLabel={modeLabel} seasonSelector={<EndlessSeasonSelector />} seasonTitle={<EndlessSeasonTitle />} />
 			<EndlessGame dataset={dataset} />
 		</>
+	);
+}
+
+function EndlessHeaderBadge() {
+	return (
+		<div className="w-full flex items-center justify-center mb-3 -mt-2">
+			<div className="flex items-center justify-center rounded-md dark:bg-primary-foreground/80 bg-primary-foreground/85 py-1 px-4 gap-2">
+				<GamepadIcon className="text-white/90 size-6" />
+				<span className="font-owl text-white/90 text-lg">Endless Mode</span>
+			</div>
+		</div>
 	);
 }
