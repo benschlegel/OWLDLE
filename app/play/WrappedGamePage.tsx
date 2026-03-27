bb1896e016e845029ac903992786475f972f1473
Ben Schlegel <ben5.schlegel@gmail.com>
2026-03-28 00:29:39 +0100

    feat: move socials badge to bottom, fix inconsistent styling + hide "endless mode" badge on mobile

--- app/play/WrappedGamePage.tsx
+++ app/play/WrappedGamePage.tsx
@@ -3,7 +3,6 @@ import UpdateMetadata from '@/app/play/UpdateMetadata';
 import GamePage from '@/components/landing-page/game-page';
 import GlobalStats from '@/components/game-container/global-stats';
 import Header from '@/components/landing-page/header';
-import Socials from '@/components/landing-page/socials';
 import { type Dataset, DATASETS } from '@/data/datasets';
 import { useSeasonParams } from '@/hooks/use-season-params';
 import { notFound } from 'next/navigation';
@@ -15,7 +14,6 @@ import LeftColumn from '@/components/game-container/left-column';
 const validSeasons = DATASETS;
 
 const MemoizedHeader = React.memo(Header);
-const MemoizedSocials = React.memo(Socials);
 
 export default function SeasonPageWrapper() {
 	const [season, _setSeason] = useSeasonParams();
@@ -27,9 +25,6 @@ export default function SeasonPageWrapper() {
 				<LeftColumn />
 				<RightColumn />
 				<UpdateMetadata />
-				<div className="sm:hidden block">
-					<MemoizedSocials />
-				</div>
 				<MemoizedHeader />
 				<GamePage slug={season} />
 			</div>
