5cd826e61999c9c41d5b64c474f232229df0baec
Ben Schlegel <ben5.schlegel@gmail.com>
2026-03-28 21:44:43 +0100

    feat: update db scripts

--- db-scripts/insertPlayers.ts
+++ db-scripts/insertPlayers.ts
@@ -2,6 +2,6 @@ import { insertPlayers } from '@/lib/databaseAccess';
 import { exit } from 'node:process';
 
 console.time('insertPlayers');
-await insertPlayers('owcs-s3');
+await insertPlayers('owcs-s2');
 console.timeEnd('insertPlayers');
 exit(0);
