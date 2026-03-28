3c5b607e9e97a0e3ec2b6c87b12e4de6f69a2070
Ben Schlegel <ben5.schlegel@gmail.com>
2026-03-28 21:44:43 +0100

    feat: update db scripts

--- db-scripts/migrateStage.ts
+++ db-scripts/migrateStage.ts
@@ -1,9 +1,13 @@
+import type { Dataset } from '@/data/datasets';
 import { migrateGameLogDataset, migrateIterationDataset, migratePlayerDataset } from '@/lib/databaseAccess';
 import { exit } from 'node:process';
 
+const migrateFromDataset: Dataset = 'owcs-s2';
+const migrateToDataset = 'owcs-s2-stage2';
+
 console.time('migrate');
-await migrateIterationDataset('owcs-s2', 'owcs-s2-stage1');
-await migrateGameLogDataset('owcs-s2', 'owcs-s2-stage1');
-await migratePlayerDataset('owcs-s2', 'owcs-s2-stage1');
+await migrateIterationDataset(migrateFromDataset, migrateToDataset);
+await migrateGameLogDataset(migrateFromDataset, migrateToDataset);
+await migratePlayerDataset(migrateFromDataset, migrateToDataset);
 console.timeEnd('migrate');
 exit(0);
