da3152204c8a36da7ab42b3aa431a8b9350a8392
Ben Schlegel <ben5.schlegel@gmail.com>
2026-03-28 20:08:12 +0100

    feat: add owcs-s1 team logos

--- data-scripts/getUniqueTeams.ts
+++ data-scripts/getUniqueTeams.ts
@@ -7,7 +7,14 @@ const DATASET: Dataset = 'owcs-s1';
 const datasetIndex = ['season1', 'season2', 'season3', 'season4', 'season5', 'season6', 'owcs-s1', 'owcs-s2', 'owcs-s3'].indexOf(DATASET);
 const players = ALL_PLAYERS[datasetIndex] ?? [];
 
-const uniqueTeams = [...new Set(players.map((p) => p.team))].sort();
+const seen = new Set<string>();
+const uniqueTeams: string[] = [];
+for (const player of players) {
+  if (!seen.has(player.team)) {
+    seen.add(player.team);
+    uniqueTeams.push(player.team);
+  }
+}
 
 console.log(`Found ${uniqueTeams.length} unique teams from ${players.length} players in '${DATASET}':\n`);
 console.log(JSON.stringify(uniqueTeams, null, 2));
