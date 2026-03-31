ae57d1102e436997f4c0a2c06ef20047cf778b0b
Ben Schlegel <ben5.schlegel@gmail.com>
2026-03-31 15:32:07 +0200

    chore: disable extractPlayers script (uses axios)

--- data-scripts/extractPlayers.ts
+++ data-scripts/extractPlayers.ts
@@ -1,7 +1,7 @@
 import type { Dataset } from '@/data/datasets';
 import { getCountryAbbreviation } from '@/types/countries';
 import { playerSchema } from '@/types/players';
-import axios from 'axios';
+// import axios from 'axios';
 import * as cheerio from 'cheerio';
 import fs from 'node:fs';
 import { exit } from 'node:process';
@@ -36,104 +36,104 @@ const blacklist = ['sinatraa', 'Aspire'];
 console.time('parse');
 
 // Function to fetch and extract player data
-async function extractPlayers() {
-	try {
-		// Fetch the HTML content of the page
-		const { data } = await axios.get(url);
+// async function extractPlayers() {
+// 	try {
+// 		// Fetch the HTML content of the page
+// 		const { data } = await axios.get(url);
 
-		// Load the HTML into cheerio for parsing
-		const $ = cheerio.load(data);
+// 		// Load the HTML into cheerio for parsing
+// 		const $ = cheerio.load(data);
 
-		// Division ids/selectors (Only select Atlantic because Pacific sibling will be automatically selected, Specify Atlantic_Division and _Conference, because 2020 season has a different id)
-		// Could also select by <span id="Participants"> and select h3 sibling children
-		const divisions = ['Atlantic_Division', 'Atlantic_Conference', 'East'];
+// 		// Division ids/selectors (Only select Atlantic because Pacific sibling will be automatically selected, Specify Atlantic_Division and _Conference, because 2020 season has a different id)
+// 		// Could also select by <span id="Participants"> and select h3 sibling children
+// 		const divisions = ['Atlantic_Division', 'Atlantic_Conference', 'East'];
 
-		const players: PlayerData[] = [];
+// 		const players: PlayerData[] = [];
 
-		// Loop through each section and extract player details
-		for (const sectionId of divisions) {
-			// Find the parent div and loop through all teamcards
-			$(`#${sectionId}`)
-				.parent()
-				.nextAll('div')
-				.find('.teamcard-inner')
-				.each(function (this) {
-					// Extract the team name from the center tag just before the .teamcard-inner div
-					const teamName = $(this).parent().find('center a').text().trim();
+// 		// Loop through each section and extract player details
+// 		for (const sectionId of divisions) {
+// 			// Find the parent div and loop through all teamcards
+// 			$(`#${sectionId}`)
+// 				.parent()
+// 				.nextAll('div')
+// 				.find('.teamcard-inner')
+// 				.each(function (this) {
+// 					// Extract the team name from the center tag just before the .teamcard-inner div
+// 					const teamName = $(this).parent().find('center a').text().trim();
 
-					// Only select the table with [data-toggle-area-content="1"] (this one actually contains the data, not images, etc)
-					const validTable = $(this).find('table[data-toggle-area-content="1"]');
+// 					// Only select the table with [data-toggle-area-content="1"] (this one actually contains the data, not images, etc)
+// 					const validTable = $(this).find('table[data-toggle-area-content="1"]');
 
-					// Loop through each player row in the valid table
-					validTable.find('tbody tr').each((index, element) => {
-						const row = $(element);
+// 					// Loop through each player row in the valid table
+// 					validTable.find('tbody tr').each((index, element) => {
+// 						const row = $(element);
 
-						// Extract the role from the image's alt attribute in the first <th> cell
-						const role = row.find('th img').attr('alt') || 'Unknown';
+// 						// Extract the role from the image's alt attribute in the first <th> cell
+// 						const role = row.find('th img').attr('alt') || 'Unknown';
 
-						// Extract the player's name from the second <td> cell
-						const name = row.find('td:nth-child(2) a').text().trim();
+// 						// Extract the player's name from the second <td> cell
+// 						const name = row.find('td:nth-child(2) a').text().trim();
 
-						// Extract the player's country from the title attribute of the <a> tag inside the flag span
-						const country = row.find('td span a img').attr('alt') || 'Unknown';
+// 						// Extract the player's country from the title attribute of the <a> tag inside the flag span
+// 						const country = row.find('td span a img').attr('alt') || 'Unknown';
 
-						// If a player's name exists, add them to the players array
-						if (name && role !== 'Unknown' && !blacklist.includes(name)) {
-							// * Parse data
-							let parsedRole = role;
-							if (parsedRole === 'DPS') {
-								parsedRole = 'Damage';
-							}
+// 						// If a player's name exists, add them to the players array
+// 						if (name && role !== 'Unknown' && !blacklist.includes(name)) {
+// 							// * Parse data
+// 							let parsedRole = role;
+// 							if (parsedRole === 'DPS') {
+// 								parsedRole = 'Damage';
+// 							}
 
-							const parsedCountry = getCountryAbbreviation(country) ?? 'Unknown';
-							// Remove whitespaces from team name
-							const parsedTeamName = teamName.replaceAll(' ', '');
+// 							const parsedCountry = getCountryAbbreviation(country) ?? 'Unknown';
+// 							// Remove whitespaces from team name
+// 							const parsedTeamName = teamName.replaceAll(' ', '');
 
-							const parsedPlayer: PlayerData = {
-								name,
-								country: parsedCountry,
-								role: parsedRole,
-								team: parsedTeamName,
-							};
+// 							const parsedPlayer: PlayerData = {
+// 								name,
+// 								country: parsedCountry,
+// 								role: parsedRole,
+// 								team: parsedTeamName,
+// 							};
 
-							if (parsedRole === 'Flex') {
-								parsedPlayer.role = 'Tank';
-								parsedPlayer.isFlex = true;
-							}
-							players.push(parsedPlayer);
-						}
-					});
-				});
-		}
+// 							if (parsedRole === 'Flex') {
+// 								parsedPlayer.role = 'Tank';
+// 								parsedPlayer.isFlex = true;
+// 							}
+// 							players.push(parsedPlayer);
+// 						}
+// 					});
+// 				});
+// 		}
 
-		// * Validate data
-		const validatePlayers = playerSchema(DATASET);
-		for (const player of players) {
-			const playerRes = validatePlayers.safeParse(player);
+// 		// * Validate data
+// 		const validatePlayers = playerSchema(DATASET);
+// 		for (const player of players) {
+// 			const playerRes = validatePlayers.safeParse(player);
 
-			// Error handling
-			if (!playerRes.success) {
-				const errMessage = playerRes.error.errors.map((err) => `${err.path}: ${err.message},`);
-				console.error(`Invalid Player. Errors: {\n${errMessage.join('\n')}\n}`);
-				exit(1);
-			}
-		}
+// 			// Error handling
+// 			if (!playerRes.success) {
+// 				const errMessage = playerRes.error.errors.map((err) => `${err.path}: ${err.message},`);
+// 				console.error(`Invalid Player. Errors: {\n${errMessage.join('\n')}\n}`);
+// 				exit(1);
+// 			}
+// 		}
 
-		// Write the players array to a JSON file
-		fs.writeFileSync('players.json', JSON.stringify(players));
+// 		// Write the players array to a JSON file
+// 		fs.writeFileSync('players.json', JSON.stringify(players));
 
-		// Log success and output the players
-		console.log('Players data has been written to players.json');
-		console.log('Found: ', players.length);
-		console.timeEnd('parse');
+// 		// Log success and output the players
+// 		console.log('Players data has been written to players.json');
+// 		console.log('Found: ', players.length);
+// 		console.timeEnd('parse');
 
-		const valueArr = players.map((item) => item.name);
-		const isDuplicate = valueArr.some((item, idx) => valueArr.indexOf(item) !== idx);
-		console.log('Found duplicates: ', isDuplicate);
-	} catch (error) {
-		console.error('Error fetching or parsing the data:', error);
-	}
-}
+// 		const valueArr = players.map((item) => item.name);
+// 		const isDuplicate = valueArr.some((item, idx) => valueArr.indexOf(item) !== idx);
+// 		console.log('Found duplicates: ', isDuplicate);
+// 	} catch (error) {
+// 		console.error('Error fetching or parsing the data:', error);
+// 	}
+// }
 
 // Run the extraction
-extractPlayers();
+// extractPlayers();
